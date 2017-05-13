6890a51d1073f238c3325e7c121face6eba34167
WoSew <wojseweryn@gmail.com>
2017-05-13 11:36:43 +0200

    Proba mikrofonu

---
+++ TociTeachersThunder/0/berserki/WoSew/test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int *wsk;
+  wsk=NULL;
+
+  if(!wsk) cout<<"Wskaznik nie jest ustawiony!";
+  if(wsk) cout <<"Wskaznik jest ustawiony";
+
+    return 0;
+}
