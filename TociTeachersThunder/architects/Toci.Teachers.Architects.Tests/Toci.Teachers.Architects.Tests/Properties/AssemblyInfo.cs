8d8b1f074e69b04a505d91c81079aed72a3803bd
warrior888 <warriorr@poczta.fm>
2017-05-27 08:48:32 +0200

    initial commit - project structure.

---
+++ TociTeachersThunder/architects/Toci.Teachers.Architects.Tests/Toci.Teachers.Architects.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Toci.Teachers.Architects.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Toci.Teachers.Architects.Tests")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("709887f6-8633-43f2-a176-d4a4e8f7fbc3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
