7ac539419cfda2dae1bfaa3803b0540c605d4638
warrior888 <warriorr@poczta.fm>
2017-06-04 20:59:50 +0200

    relative path fiul eaccesss tests.

---
+++ TociTeachersThunder/architects/Toci.Teachers.Architects.Tests/Toci.Teachers.Architects.Tests/GhostRider/FileAccess/PeselValidatorsTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Toci.Teachers.Architects.Tests.GhostRider.FileAccess
+{
+    [TestClass]
+    public class PeselValidatorsTests
+    {
+        protected virtual List<string> GetFileContents()
+        {
+            string path = @"..\..\..\..\Toci.Teachers.Architects.Tests\Toci.Teachers.Architects.Tests\GhostRider\FileAccess\data\peselcandidates.txt";
+
+            StreamReader sr = new StreamReader(path);
+
+            sr.ReadToEnd();
+
+            // split liste itd
+
+            return null;
+        }
+
+        [TestMethod]
+        public void PeselValidationTest()
+        {
+            GetFileContents();
+        }
+    }
+}
