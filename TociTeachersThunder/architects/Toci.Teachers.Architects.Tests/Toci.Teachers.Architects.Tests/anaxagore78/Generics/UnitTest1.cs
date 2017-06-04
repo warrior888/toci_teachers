8cac50b695c6cac8222c73799c05af311a2af767
anaxagore78 <anaxagore78@DESKTOP-E1L00OL>
2017-06-04 22:43:21 +0200

    homeWork refactored

--- TociTeachersThunder/architects/Toci.Teachers.Architects.Tests/Toci.Teachers.Architects.Tests/anaxagore78/Generics/UnitTest1.cs
+++ TociTeachersThunder/architects/Toci.Teachers.Architects.Tests/Toci.Teachers.Architects.Tests/anaxagore78/Generics/UnitTest1.cs
@@ -2,6 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Toci.Architects.Training.anaxagore78.Homework.PeselValidator;
 using Toci.Architects.Training.Interfaces.GhostRider.Extending.PeselValidator;
+using System.Collections.Generic;
 
 namespace Toci.Teachers.Architects.Tests.anaxagore78.Generics
 {
@@ -15,12 +16,53 @@ namespace Toci.Teachers.Architects.Tests.anaxagore78.Generics
             string pesel = "78061906751"; //poprawny
             //string pesel = "80090102444"; //poprawny
             IPeselValidator validator = new PeselValidatorAnaxagore78();
-            //Console.WriteLine($"{pesel}- {((validator.IsPeselValid(pesel)) ? "poprawny" : "niepoprawny")} numer pesel");
-            PeselValidatorAnaxagore78 validatorV2 = new PeselValidatorAnaxagore78();
-            Console.WriteLine(validatorV2.GetSex(pesel));
+            validator.IsPeselValid(pesel);
+            //Console.WriteLine("{0} - {1} numer pesel", pesel, ((validator.IsPeselValid(pesel)) ? "poprawny" : "niepoprawny"));
+        }
+
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            IReflectionAggregator aggregator = new ReflectionAggregatorAnaxagore78();
+            Dictionary<string, IPeselValidator> validators = aggregator.GetAllPeselValidators();
+
+        }
+
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            //string pesel = "78061906750"; //bledny
+            string pesel = "78061906751"; //poprawny
+            //string pesel = "80090102444"; //poprawny
+
+            IPeselValidator peselValidator = new PeselValidatorAnaxagore78();
+            Console.WriteLine("{0} -{1} numer pesel", pesel,
+                ((peselValidator.IsPeselValid(pesel)) ? "poprawny" : "niepoprawny"));
 
             IReflectionAggregator aggregator = new ReflectionAggregatorAnaxagore78();
-            aggregator.GetAllPeselValidators();
+
+            Dictionary<string, IPeselValidator> validators = aggregator.GetAllPeselValidators();
+
+            Dictionary<string, List<string>> validatorsStatus = new Dictionary<string, List<string>>();
+
+            foreach (var validator in validators)
+            {
+                //try
+                //{
+                //    validator.Value.IsPeselValid(pesel);
+                //}
+                //catch (NotImplementedException notImplemented)
+                //{
+                //    validatorsStatus.Add(validator.Key,);
+                //}
+                //catch (Exception exc)
+                //{
+
+                //}
+
+            }
         }
     }
 }
--- TociTeachersThunder/architects/Toci.Teachers.Architects.Trainings/Toci.Teachers.Architects.Trainings/anaxagore78/Homework/PeselValidator/PeselValidatorAnaxagore78.cs
+++ TociTeachersThunder/architects/Toci.Teachers.Architects.Trainings/Toci.Teachers.Architects.Trainings/anaxagore78/Homework/PeselValidator/PeselValidatorAnaxagore78.cs
@@ -1,81 +1,39 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
 using Toci.Architects.Training.Interfaces.GhostRider.Extending.PeselValidator;
 
 namespace Toci.Architects.Training.anaxagore78.Homework.PeselValidator
 {
-
     public class PeselValidatorAnaxagore78 : IPeselValidator
     {
-        public enum Sex { Male, Female }
-
         public string Name()
         {
             return "anaxagore78";
         }
-
-        /*
-         Informacja o płci osoby, której zestaw informacji jest identyfikowany, zawarta jest na 10. (przedostatniej) pozycji numeru PESEL.
-
-    cyfry 0, 2, 4, 6, 8 – oznaczają płeć żeńską
-    cyfry 1, 3, 5, 7, 9 – oznaczają płeć męską
-
-             */
-
-        public Sex GetSex(string pesel)
-        {
-            if (!IsPeselValid(pesel))
-            {
-                throw new Exception(pesel + " nie jest poprawnym numerem PESEL, brak mozliwosci okreslenia płci");
-            }
-            int sexDigit = int.Parse(pesel[9].ToString());
-            return sexDigit % 2 == 0 ? Sex.Female : Sex.Male;
-        }
-
-
         public bool IsPeselValid(string pesel)
         {
             if (!(pesel.Trim().Length.Equals(11))) return false;  //sprawdzamy czy dlugosc ciagu to 11 znakow
-
             char[] chars = pesel.ToCharArray();
-
             if (!(chars.Where(char.IsDigit).Count().Equals(11))) //sprawdzamy czy ciag zawiera 11 cyfr
             {
-                //throw new Exception("Numer pesel musi składać sie z 11 cyfr");
-                return false;
+                return false; //throw new Exception("Numer pesel musi składać sie z 11 cyfr");
             }
+            int controlDigit = int.Parse(pesel[10].ToString());
+            int controlSum = GetControlSum(pesel);
+            return (controlSum % 10).Equals(controlDigit);
+        }
 
+        private static int GetControlSum(string pesel)
+        {
             int sum = 0;
-            int evenIndx = 0;
-            int[] evens = new int[4] { 9, 7, 3, 1 };
-            int controlDigit = int.Parse(pesel[10].ToString()); //cyfra kontrolna
-
+            List<int> weights = new List<int> { 9, 7, 3, 1 };
             for (int i = 0; i < 10; i++)
             {
-                sum += int.Parse(pesel[i].ToString()) * evens[evenIndx++];
-                if (evenIndx >= evens.Length) evenIndx = 0;
+                sum += int.Parse(pesel[i].ToString()) * weights[i % 4];
             }
-
-            return (sum % 10).Equals(controlDigit);
-            /*
-             Jedenasta cyfra jest cyfrą kontrolną, służącą do wychwytywania przekłamań numeru. Jest ona generowana na podstawie pierwszych dziesięciu cyfr. Aby sprawdzić czy dany numer PESEL jest prawidłowy, należy, zakładając, że litery a-j to kolejne cyfry numeru od lewej, obliczyć wyrażenie:
-
-            9×a + 7×b + 3×c + 1×d + 9×e + 7×f + 3×g + 1×h + 9×i + 7×j
-
-            Jeżeli ostatnia cyfra otrzymanego wyniku nie jest równa cyfrze kontrolnej, to znaczy, że numer zawiera błąd[15].
-
-            Przykład dla numeru PESEL 44051401358:
-
-                9×4 + 7×4 + 3×0 + 1×5 + 9×1 + 7×4 + 3×0 + 1×1 + 9×3 + 7×5 = 169
-
-            Wyznaczamy resztę z dzielenia sumy przez 10:
-
-                169:10 = 16 reszta = 9
-
-            Wynik 9 nie jest równy ostatniej cyfrze numeru PESEL, czyli 8, więc numer jest błędny.
-             
-             */
+            return sum;
         }
     }
 }
