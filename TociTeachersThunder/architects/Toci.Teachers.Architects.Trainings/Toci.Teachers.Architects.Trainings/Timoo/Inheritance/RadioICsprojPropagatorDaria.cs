c74934c913696d25488bee8106d0f59fe45f3ef6
Daria(Timo) <dariia.ratynska@gmail.com>
2017-06-04 10:46:12 +0200

    RadioICsprojPropagatorDaria - zadanie 3

---
+++ TociTeachersThunder/architects/Toci.Teachers.Architects.Trainings/Toci.Teachers.Architects.Trainings/Timoo/Inheritance/RadioICsprojPropagatorDaria.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toci.Architects.Training.Interfaces.GhostRider.Inheritance;
+
+namespace Toci.Architects.Training.Timoo.Inheritance
+{
+    public class RadioICsprojPropagatorDaria : ICsprojPropagator
+    {
+        public bool CanPropagateCsproj(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendCharacter(int position, string text)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
