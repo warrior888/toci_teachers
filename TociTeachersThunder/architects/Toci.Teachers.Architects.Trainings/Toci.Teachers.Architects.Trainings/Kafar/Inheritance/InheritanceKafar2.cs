08228cf2e9dbf44bf8a2b41b41eb0d41d8134162
MKafar <mlotek92@gmail.com>
2017-06-04 09:54:14 +0200

    2nd task

---
+++ TociTeachersThunder/architects/Toci.Teachers.Architects.Trainings/Toci.Teachers.Architects.Trainings/Kafar/Inheritance/InheritanceKafar2.cs
@@ -0,0 +1,17 @@
+﻿using Toci.Architects.Training.Interfaces.GhostRider.Inheritance;
+
+namespace Toci.Architects.Training.Kafar.Inheritance
+{
+    public class InheritanceKafar2 : ICsprojPropagator
+    {
+        public void SendCharacter(int position, string text)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool CanPropagateCsproj(string path)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
