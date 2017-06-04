1886f9447460c88b3baf70f7285b5b7c16c51ee6
Piotr Mazur <piotr.mazur.priv@gmail.com>
2017-06-04 10:08:50 +0200

    Dodanie klasy

--- TociTeachersThunder/architects/Toci.Teachers.Architects.Trainings/Toci.Teachers.Architects.Trainings/Piotrek/Inheritance/InheritanceIz.cs
+++ TociTeachersThunder/architects/Toci.Teachers.Architects.Trainings/Toci.Teachers.Architects.Trainings/Piotrek/Inheritance/InheritanceIz.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Toci.Architects.Training.Interfaces.GhostRider.Inheritance;
 
 namespace Toci.Architects.Training.Piotrek.Inheritance
 {
-    class InheritanceIz
+    class InheritancePiotrek : IPlugin
     {
+        public void SendCharacter(int position, string text)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
