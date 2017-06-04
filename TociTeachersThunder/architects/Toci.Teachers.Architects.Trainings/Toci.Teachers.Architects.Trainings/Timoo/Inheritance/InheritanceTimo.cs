4b80af15228bb51a71b6e07d4b16457732c5730a
DESKTOP-MEBKDTB\Timo <dariia.ratynska@gmail.com>
2017-06-04 09:40:25 +0200

    Inheritance Daria(Timo)

---
+++ TociTeachersThunder/architects/Toci.Teachers.Architects.Trainings/Toci.Teachers.Architects.Trainings/Timoo/Inheritance/InheritanceTimo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toci.Architects.Training.Interfaces.GhostRider.Inheritance;
+
+namespace Toci.Architects.Training.Timoo.Inheritance
+{
+    public class InheritanceTimo : IPlugin
+    {
+        public void SendCharacter(int position, string text)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
