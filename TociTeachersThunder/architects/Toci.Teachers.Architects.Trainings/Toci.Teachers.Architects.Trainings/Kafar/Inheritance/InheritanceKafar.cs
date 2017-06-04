4478e1910dcac9abc7c37a42546da888b45f6ea2
MKafar <mlotek92@gmail.com>
2017-06-04 09:11:10 +0200

    Inheritance Kafar.

---
+++ TociTeachersThunder/architects/Toci.Teachers.Architects.Trainings/Toci.Teachers.Architects.Trainings/Kafar/Inheritance/InheritanceKafar.cs
@@ -0,0 +1,12 @@
+﻿using Toci.Architects.Training.Interfaces.GhostRider.Inheritance;
+
+namespace Toci.Architects.Training.Kafar.Inheritance
+{
+    public class InheritanceKafar : IPlugin
+    {
+        public void SendCharacter(int position, string text)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
