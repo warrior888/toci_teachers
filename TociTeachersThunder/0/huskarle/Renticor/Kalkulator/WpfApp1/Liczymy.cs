c3f9d0a359106769f9d7e0dc4af2ae0d2f012f24
Renticor <leszek.samburski@gmail.com>
2017-06-18 00:20:39 +0200

    Kalkulator

---
+++ TociTeachersThunder/0/huskarle/Renticor/Kalkulator/WpfApp1/Liczymy.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class Liczymy
+    {
+        public double a = 0.0d;
+        public double b = 0.0d;
+        public char dzialanie ;
+
+        public double licz()
+        {
+            if (dzialanie == '+')
+                return a + b;
+            if (dzialanie == '-')
+                return a - b;
+            if (dzialanie == '*')
+                return a * b;
+            if (dzialanie == '/')
+                return a / b;
+            if (dzialanie == '^')
+                return Math.Pow(a, b);//znaku ^ nie można zastosować między dwiema liczbami typu double
+            if (dzialanie == '!')
+            {
+                long s = 1;
+                for (int i = 1; i <= a; ++i)
+                    s *= i;
+                return (double)s;
+            }
+            else
+                return 0;
+        }
+    }
+}
---
+++ TociTeachersThunder/0/huskarle/Renticor/Kalkulator/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        Liczymy liczenie = new Liczymy();
+        bool gdzieWpisac = true;
+        bool czyWyczyscicPasekWyniku = false;
+
+        private void liczba_Click(object sender, RoutedEventArgs e)
+        {
+            if (czyWyczyscicPasekWyniku)
+                pasekWyniku.Text = (sender as Button).Content.ToString();
+            else
+                pasekWyniku.Text += (sender as Button).Content.ToString();
+            czyWyczyscicPasekWyniku = false;
+        }
+
+        private void przecinek_Click(object sender, RoutedEventArgs e)
+        {
+            if (czyWyczyscicPasekWyniku)
+                pasekWyniku.Text = "0,";
+            else
+                pasekWyniku.Text += ",";
+            czyWyczyscicPasekWyniku = false;
+        }
+        private void POWIE(object sender, RoutedEventArgs e)
+        {
+            pasekWyniku.Text = "Kocham Agnieszke";
+            czyWyczyscicPasekWyniku = false;
+        }
+
+        private void dzialanie_Click(object sender, RoutedEventArgs e)
+        {
+            liczenie.dzialanie = (sender as Button).Content.ToString()[0];
+            czyWyczyscicPasekWyniku = true;//czyścimy pasek tekstowy
+            gdzieWpisac = false;
+        }
+        private void silnia_Click(object sender, RoutedEventArgs e)
+        {
+
+            liczenie.dzialanie = '!';
+            gdzieWpisac = true;
+            liczenie.a = liczenie.licz();
+            pasekWyniku.Text = liczenie.a.ToString();//wyświetlamy wynik
+            czyWyczyscicPasekWyniku = true;
+        }
+        private void pasekWyniku_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                if (gdzieWpisac)
+                    liczenie.a = double.Parse(pasekWyniku.Text);
+                else
+                    liczenie.b = double.Parse(pasekWyniku.Text);
+            }
+            catch (FormatException )//jeśli wprowadzono błędnie liczbę wykonuje poniższy kod
+            {
+                if (pasekWyniku.Text != "")
+                    MessageBox.Show("I masz wirusa LoL :p", "Błąd", MessageBoxButton.OK, MessageBoxImage.Error);//komunikat o błędzie, ale tylko, jeśli pole nie jest puste
+            }
+        }
+
+        private void czyszczenie_Click(object sender, RoutedEventArgs e)
+        {
+            liczenie.a = liczenie.b = 0;
+            liczenie.dzialanie = ' ';
+            pasekWyniku.Text = "";
+            czyWyczyscicPasekWyniku = false;
+            gdzieWpisac = true;
+        }
+
+        private void systemBinarny_Click(object sender, RoutedEventArgs e)
+        {
+            pasekWyniku.Text = Convert.ToString((int)liczenie.a, 2);
+        }
+
+        private void rowna_Click(object sender, RoutedEventArgs e)
+        {
+            pasekWyniku.Text = liczenie.licz().ToString();
+            czyWyczyscicPasekWyniku = false;
+        }
+    }
+}
