2879fb5969da2cd22526b4a4590e7210112955d2
Bartek412 <bartekg412@gmail.com>
2017-06-24 13:50:24 +0200

    praca domwa

--- StartTraining/Toci.Training.Teachers/Toci.Training.BartekG/Program.cs
+++ StartTraining/Toci.Training.Teachers/Toci.Training.BartekG/Program.cs
@@ -10,6 +10,32 @@ namespace Toci.Training.BartekG
     {
         static void Main(string[] args)
         {
+            string[] table = new string[10];
+            int na = table.Length - 1;
+            for (int i = 0; i < table.Length; i++)
+            {
+                Console.WriteLine("Wpisz coś do indexu " + i + " na " + na);
+                table[i] = Console.ReadLine();
+
+
+            }
+            Console.WriteLine("Uzupełniłeś całą tablicę. Oto twoja tablica:");
+            for (int i = 0; i < table.Length; i++)
+            {
+                Console.WriteLine(table[i]);
+
+
+
+
+            }
+            Console.ReadLine();
+
         }
+
+
+
+
+
+
     }
 }
