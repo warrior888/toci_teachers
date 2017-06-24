cd9bc3f7e4037e71e1b4c80bff56d1a0ac279872
GhostRider <bzapart@gmail.com>
2017-06-24 12:07:34 +0200

    first toci teachers training.

---
+++ StartTraining/Toci.Training.Teachers/Toci.Training.Teachers/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toci.Training.Teachers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // typ, zmienna tablica , operator, debugowanie, ins log, blokow kodu, petli, kawa, zakres
+            // kody ascii
+            // 0 - 48ascii
+            //a - 97
+            // A - 65
+            // translacje duze male litery na tabloicy kodow ascii 
+            // utf8
+
+            string example = "Bartłomiej";
+
+            for (int i = example.Length - 1; i >= 0; i--)
+            {
+                if (example[i] != 'i')
+                {
+                    Console.Write(example[i]);
+                }
+            }
+
+            int[] lengthsOfStrings = new int[20];
+            //lengthsOfStrings[0] = 2;
+
+            int j = 0;
+            j = j + 1; // j++
+            int limit = lengthsOfStrings.Length;
+
+            for (int i = 0; i < limit; i++)
+            {
+                Console.WriteLine("I wynosi: " + i);
+
+                lengthsOfStrings[i] = ("I wynosi: " + i).Length;
+
+                if (i > 5)
+                {
+                    
+                }
+
+//                i++;
+            }
+
+            Console.WriteLine("Podaj cos czlowieku: ");
+            string valueFromUserInput = Console.ReadLine();
+            Console.WriteLine("Podaj drugie cos czlowieku: ");
+            string secondValueFromUserInput = Console.ReadLine();
+
+            if (valueFromUserInput.Length == secondValueFromUserInput.Length) // whatever -> true
+            {
+                //Debug.WriteLine();
+                Console.WriteLine("pierwszy if sie wykonuje");
+                int ofCourse = 6;
+
+                if (valueFromUserInput.Length > 3) // 0 a 1
+                {
+                    int w = 5;
+
+                }
+            }
+            else if (valueFromUserInput.Length == 5)
+            {
+                
+            }
+            else if (secondValueFromUserInput.Length == 12)
+            {
+                
+            }
+            else
+            {
+                
+            }
+
+            //valueFromUserInput = "";
+
+            string name = "źródło"; // ToUpper -> BARTEK
+            //name.Length 
+            // + - * / ++ -- += -= *= /= && || % ^ != == > < >= <=  ? :
+            int test = "Bartek " == "Bartek" ? 7 : 8;
+
+            bool robotnik = 4 > 5;
+
+            int elchu = 0;
+
+            //elchu =+ 9;
+
+            //  x -> 
+
+            bool thisIsTrue = false; // 0 true -> 1
+            
+
+            //string źródło = "";
+
+            string letterA = "a";
+
+            string lettera = "a";
+
+            int countOfA = 1;
+            int nameLength = 6;
+
+            Console.WriteLine("Skonczylem");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ StartTraining/Toci.Training.Teachers/Toci.Training.Teachers/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Toci.Training.Teachers")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Toci.Training.Teachers")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3ca7ca40-2437-4bec-84c5-1a6455d5e0ba")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
