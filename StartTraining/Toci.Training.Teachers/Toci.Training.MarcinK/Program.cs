e592d8fb68a4240aef6253a853c31c48eece15ff
Marcin-Kurpiewski <kurus.mk@gmail.com>
2017-06-24 13:07:27 +0200

    no message

---
+++ StartTraining/Toci.Training.Teachers/Toci.Training.MarcinK/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toci.Training.MarcinK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //test
+        }
+    }
+}
