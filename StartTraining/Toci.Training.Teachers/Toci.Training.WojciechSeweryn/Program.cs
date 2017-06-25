db697659aae1199f8a87c57060fd3b8113ab16a8
WoSew <wojseweryn@gmail.com>
2017-06-25 14:11:50 +0200

    homework

--- StartTraining/Toci.Training.Teachers/Toci.Training.WojciechSeweryn/Program.cs
+++ StartTraining/Toci.Training.Teachers/Toci.Training.WojciechSeweryn/Program.cs
@@ -10,6 +10,34 @@ namespace Toci.Training.WojciechSeweryn
     {
         static void Main(string[] args)
         {
+            string[] tableOfStrings = new string[10];
+
+            for (int i = 0; i < tableOfStrings.Length; i++)
+            {
+                Console.WriteLine("Prosze podaj " + (i + 1) + " linie tekstu, a ja wpisze ja do tablicy: ");
+                tableOfStrings[i] = Console.ReadLine();
+                if (i == tableOfStrings.Length - 1)
+                {
+                    Console.WriteLine("No i sie skonczylo");
+                }
+            }
+
+            //Zamiana liter na male/duze przy użyciu tablicy kodów ASCI
+            Console.WriteLine("Poda wyraz: ");
+            string sizeLetter = Console.ReadLine();
+
+            for (int i = 0; i < sizeLetter.Length; i++)
+            {
+                if ((int)sizeLetter[i] >= 65 && (int)sizeLetter[i] <= 90)
+                {
+                    Console.Write((char)((int)(sizeLetter[i] + 32)));
+                }
+                else
+                {
+                    Console.Write((char)((int)(sizeLetter[i] - 32)));
+                }
+            }
+            Console.ReadLine();
         }
     }
 }
