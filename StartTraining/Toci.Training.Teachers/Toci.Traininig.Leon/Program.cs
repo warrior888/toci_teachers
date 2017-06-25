0165f40c20f298c38080ba92a516f417be2208b7
Home\Home PL <ucios@o2.pl>
2017-06-25 12:05:30 +0200

    ToUpper

--- StartTraining/Toci.Training.Teachers/Toci.Traininig.Leon/Program.cs
+++ StartTraining/Toci.Training.Teachers/Toci.Traininig.Leon/Program.cs
@@ -10,6 +10,36 @@ namespace Toci.Traininig.Leon
     {
         static void Main(string[] args)
         {
+            ASCIIEncoding ascii = new ASCIIEncoding();
+            string text = "t";
+
+            Byte[] encodeBytes = ascii.GetBytes(text);
+            Console.WriteLine(text);
+            Console.WriteLine("The encoding: ");
+
+            
+            foreach(byte b in encodeBytes)
+            {
+                int a = 0;
+                if (b >= 97 && b <= 122)
+                {
+                    a = b;
+                    a -= 32;
+                    a = Convert.ToByte(a);
+                    //... wymyslec
+                }
+                Console.Write("[{0}]", b);
+                Console.WriteLine();
+                Console.Write("[{0}]", a);
+                char decodestring = ascii.GetString(a); // i jak to obejsc?
+            }
+            Console.WriteLine();
+
+            string decodeString = ascii.GetString(encodeBytes);
+            Console.WriteLine("Decodeing: ");
+            Console.Write(decodeString);
+
+            Console.ReadLine();
         }
     }
 }
