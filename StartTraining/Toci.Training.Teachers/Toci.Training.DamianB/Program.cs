6d8d49524f93d65c594f153f79ac6da535ca28b2
memekpl <jamtubyl@gmail.com>
2017-06-25 19:03:45 +0200

    Homework done

--- StartTraining/Toci.Training.Teachers/Toci.Training.DamianB/Program.cs
+++ StartTraining/Toci.Training.Teachers/Toci.Training.DamianB/Program.cs
@@ -10,7 +10,23 @@ namespace Toci.Training.DamianB
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("dd");
+
+            String NumberOfEntries;
+            Console.WriteLine("Ile wpisów?");
+            NumberOfEntries = Console.ReadLine();
+            String[] DataTable = new String[Convert.ToInt32(NumberOfEntries)];
+            for (int i = 0; i < Convert.ToInt32(NumberOfEntries); i++)
+            {
+                Console.WriteLine("Podaj wpis: ");
+                DataTable[i] = Console.ReadLine();
+                //poniżej sprawdzałem czy sie uzupełnia tablica i co zostało wpisane
+                // Console.WriteLine("Wpisano " + DataTable[i]);
+
+            }
+            Console.WriteLine("Osiągnięto maksymalną ilość wpisów: {0}", NumberOfEntries);
+            Console.WriteLine("wciśnij enter...");
+            Console.ReadLine();
+
         }
     }
 }
