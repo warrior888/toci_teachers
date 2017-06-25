d3083670e64351e53d0b0aeed12be99f2079a538
MKafar <mlotek92@gmail.com>
2017-06-25 12:45:08 +0200

    unimportant

--- StartTraining/Toci.Training.Teachers/Toci.Traininig.Leon/Program.cs
+++ StartTraining/Toci.Training.Teachers/Toci.Traininig.Leon/Program.cs
@@ -17,13 +17,12 @@ namespace Toci.Traininig.Leon
             Console.WriteLine(text);
             Console.WriteLine("The encoding: ");
 
-            
             foreach(byte b in encodeBytes)
             {
                 int a = 0;
                 if (b >= 97 && b <= 122)
                 {
-                    a = b;
+;                    a = b;
                     a -= 32;
                     a = Convert.ToByte(a);
                     //... wymyslec
@@ -31,7 +30,7 @@ namespace Toci.Traininig.Leon
                 Console.Write("[{0}]", b);
                 Console.WriteLine();
                 Console.Write("[{0}]", a);
-                char decodestring = ascii.GetString(a); // i jak to obejsc?
+                //char decodestring = ascii.GetString(a); // i jak to obejsc?
             }
             Console.WriteLine();
 
