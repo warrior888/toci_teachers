1bc8e2f79bd8d13214d26522cb359abc16dc5dc2
Cudny-PC\Cudny <cudny4321@gmail.com>
2017-06-26 22:32:31 +0200

    MarekC

--- StartTraining/Toci.Training.Teachers/MarekCudny/Program.cs
+++ StartTraining/Toci.Training.Teachers/MarekCudny/Program.cs
@@ -10,6 +10,27 @@ namespace MarekCudny
     {
         static void Main(string[] args)
         {
+            // napisac translacje z malych na duze litery kozystajac z tablicy kodow ASCI
+            string TextToChange = "";
+            TextToChange = Console.ReadLine();
+            ChangeCaseToOpposite(TextToChange);
+        }
+
+        private static void ChangeCaseToOpposite(string s)
+        {
+            foreach (char c in s)
+            {
+                if ((int)c >= 65 && (int)c <= 90)
+                {
+                    Console.Write((char)((int)c + 32)); //change to lower case
+                }
+                else
+                {
+                    Console.Write((char)((int)c - 32)); //change to upper case
+                }
+
+            }
+            Console.ReadKey();
         }
     }
 }
