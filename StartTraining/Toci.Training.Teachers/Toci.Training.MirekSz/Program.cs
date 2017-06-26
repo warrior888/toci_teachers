c96cb2bccc9bf43267771e76cf4ff1651a9a4b7d
poznanprogramowanie <irobotyka@gmail.com>
2017-06-26 23:37:41 +0100

    if

--- StartTraining/Toci.Training.Teachers/Toci.Training.MirekSz/Program.cs
+++ StartTraining/Toci.Training.Teachers/Toci.Training.MirekSz/Program.cs
@@ -10,14 +10,33 @@ namespace Toci.Training.MirekSz
     {
         static void Main(string[] args)
         {
-            string name = "MirekSz".ToLower();
-            //int variabelLength = 7;
-            int x = 5;
-            int y = 60;
-            bool test = x == y;
-            string name = "Mirek";
-            Console.Write(y);
+            
+
+            string name = "Mi";
+            Console.Write(name + " ");
+            Console.Write("Sz\n");
+
+            if (name.Length > 3)
+            {
+                Console.Write("Twoje imię ma wiecej niż 3 litery");
+            } else
+            {
+                Console.Write("Twoje imię ma mniej niż 4 litery");
+            }
+
             Console.ReadLine();
+
+
+
+
+
+
+            //string name = "MirekSz".ToLower();
+            //int variabelLength = 7;
+            //int x = 5;
+            //int y = 60;
+            //bool test = x == y;
+
             // Console.ReadKey();
 
         }
