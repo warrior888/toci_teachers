8d667afcac1ef48e9959edb45f8a54d098d6d059
Pawel_Kownacki <pkownacki90@gmail.com>
2017-06-26 14:12:37 +0200

    Write/Read table

--- StartTraining/Toci.Training.Teachers/Toci.Training.PawelK/Program.cs
+++ StartTraining/Toci.Training.Teachers/Toci.Training.PawelK/Program.cs
@@ -10,6 +10,20 @@ namespace Toci.Training.PawelK
     {
         static void Main(string[] args)
         {
+
+            string [] text = new string[5];
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                text[i] = Console.ReadLine();
+            }
+
+
+            for (int i = 0; i < text.Length;i++)
+            {
+                Console.WriteLine(i + 1 + ". " + text[i]);
+            }
+            Console.ReadKey();
         }
     }
 }
