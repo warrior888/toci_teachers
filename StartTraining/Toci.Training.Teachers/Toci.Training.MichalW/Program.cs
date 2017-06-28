9679cd69a08a260277ef319440a4c479bebb64cb
MICHAL\useru <majer20@wp.pl>
2017-06-28 20:05:14 +0200

    zad 1

--- StartTraining/Toci.Training.Teachers/Toci.Training.MichalW/Program.cs
+++ StartTraining/Toci.Training.Teachers/Toci.Training.MichalW/Program.cs
@@ -10,34 +10,61 @@ namespace Toci.Training.MichalW
     {
         static void Main(string[] args)
         {
-           Console.WriteLine("Podaj słowo : ");
+            /*Console.WriteLine("Podaj słowo : ");
 
-            string InputWord = Console.ReadLine();
+             string InputWord = Console.ReadLine();
 
-            ASCIIEncoding ascii = new ASCIIEncoding();
+             ASCIIEncoding ascii = new ASCIIEncoding();
 
-            Byte[] ASCIIValues = ascii.GetBytes(InputWord);
-            foreach (var Number in ASCIIValues)
+             Byte[] ASCIIValues = ascii.GetBytes(InputWord);
+             foreach (var Number in ASCIIValues)
+             {
+                 if (Number >= 97 && Number <= 122)
+                 {
+
+                     int result = Number - 32;
+                     result = Convert.ToByte(result);
+
+
+                     Console.WriteLine("[{0}]", result);
+                     Console.WriteLine();
+                     String decoded = ascii.GetString(ASCIIValues);
+                     Console.WriteLine("Decoded string: '{0}'", decoded);
+
+                    // Nie pokazuje duzych liter mimo zmiany kodu ASCII...
+                 }
+             }
+
+
+
+             Console.ReadKey();*/
+
+            Console.Write("Ilu elementowa ma być Twoja tablica : ");
+            string InputUser = Console.ReadLine();
+            int UserInputNumber = 0;
+            int.TryParse(InputUser, out UserInputNumber);
+            int[] example = new int[UserInputNumber];
+
+            string[] Table = new string[UserInputNumber];
+            int number = Table.Length;
+
+            for (int i = 0; i < number; i++)
             {
-                if (Number >= 97 && Number <= 122)
-                {
-                    
-                    int result = Number - 32;
-                    result = Convert.ToByte(result);
-                    
-                    
-                    Console.WriteLine("[{0}]", result);
-                    Console.WriteLine();
-                    String decoded = ascii.GetString(ASCIIValues);
-                    Console.WriteLine("Decoded string: '{0}'", decoded);
+                Console.Write("\nPodaj element nr " + (i + 1) + ": ");
+                Table[i] = Console.ReadLine();
 
-                   // Nie pokazuje duzych liter mimo zmiany kodu ASCII...
+                if (number - 1 == i)
+                {
+                    Console.WriteLine("\nUzupełniłeś swoją tablicę !");
                 }
             }
+            Console.WriteLine("\nElementy Twojej tablicy :");
 
-
-                
-            
+            for (int i = 0; i < number; i++)
+            {
+                Console.Write(" " + (i + 1) + "." + Table[i]);
+            }
+            Console.ReadKey();
         }
     }
 }
