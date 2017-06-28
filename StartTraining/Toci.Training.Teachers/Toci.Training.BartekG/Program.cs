0d17534ad3b0b5c35316068954c7f1af1cc926e7
Bartek412 <bartekg412@gmail.com>
2017-06-28 11:42:13 +0200

    homework

--- StartTraining/Toci.Training.Teachers/Toci.Training.BartekG/Program.cs
+++ StartTraining/Toci.Training.Teachers/Toci.Training.BartekG/Program.cs
@@ -9,28 +9,98 @@ namespace Toci.Training.BartekG
     class Program
     {
         static void Main(string[] args)
-        {
-            string[] table = new string[10];
-            int na = table.Length - 1;
-            for (int i = 0; i < table.Length; i++)
-            {
-                Console.WriteLine("Wpisz coś do indexu " + i + " na " + na);
-                table[i] = Console.ReadLine();
+        {     
 
-                if (i==table.Length-1)
+            // wczytywanie do tablicy
+             string[] table = new string[10];
+             int na = table.Length - 1;
+             for (int i = 0; i < table.Length; i++)
+             {
+                 Console.WriteLine("Wpisz coś do indexu " + i + " na " + na);
+                 table[i] = Console.ReadLine();
+
+                 if (i==table.Length-1)
+                 {
+                     Console.WriteLine("Uzupełniłeś całą tablicę :)");
+                 }
+             }
+             Console.WriteLine("Oto twoja tablica: ");
+             for (int i = 0; i < table.Length; i++)
+             {
+                 Console.WriteLine("dla ["+i+"] = "+table[i]);
+
+
+
+
+             }
+            // max liczba
+            int[] homework = new int[11]; //[3, 56, 3, 5, 78, 9, 78, 4, 3, 567, 8] 
+            homework[0] = 3; 
+            homework[1] = 56;
+            homework[2] = 3;
+            homework[3] = 5;
+            homework[4] = 78;
+            homework[5] = 9;
+            homework[6] = 78;
+            homework[7] = 4;
+            homework[8] = 3;
+            homework[9] = 567;
+            homework[10] = 8;
+
+            int max = homework[0];
+
+        
+            for (int i = 0; i <homework.Length ; i++)
+            {
+                if (homework[i]>max)
                 {
-                    Console.WriteLine("Uzupełniłeś całą tablicę :)");
+                     max = homework[i];
                 }
+            
             }
-            Console.WriteLine("Oto twoja tablica: ");
-            for (int i = 0; i < table.Length; i++)
+            Console.WriteLine(max);
+
+            // mininmalna wartosc
+            int min = homework[0];
+
+
+            for (int i = 0; i < homework.Length; i++)
             {
-                Console.WriteLine("dla ["+i+"] = "+table[i]);
-
-
-
+                if (homework[i] < min)
+                {
+                    min = homework[i];
+                }
 
             }
+            Console.WriteLine(min);
+
+            // Suma wszystkich elementów
+            int suma = 0;
+
+
+            for (int i = 0; i < homework.Length; i++)
+            {
+                suma += homework[i];
+
+            }
+            Console.WriteLine(suma);
+
+          
+            //ilosc  wystapien  
+
+            int wystapienia = 0;
+            int liczba = 78;
+
+
+            for (int i = 0; i < homework.Length; i++)
+            {
+                if (liczba==homework[i])
+                {
+                    wystapienia ++;
+                }
+
+            }
+            Console.WriteLine("ilosc powtorzen: " + wystapienia + " dla liczby "+ liczba );
             Console.ReadKey();
 
         }
