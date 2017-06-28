e1af36ef30bd4ae864814dcbd346a4e4c88f1e31
poznanprogramowanie <irobotyka@gmail.com>
2017-06-28 16:43:02 +0100

    zad 1

--- StartTraining/Toci.Training.Teachers/Toci.Training.MirekSz/Program.cs
+++ StartTraining/Toci.Training.Teachers/Toci.Training.MirekSz/Program.cs
@@ -20,23 +20,82 @@ namespace Toci.Training.MirekSz
             Console.Write("Ile elementów ma mieć tablica?\n");
 
             string rozmiarTablicy = Console.ReadLine();
-            int x = Int32.Parse(rozmiarTablicy);
+            
+            int x = int.Parse(rozmiarTablicy);  // rzutowanie zmiennej string na int
 
-            //string[] kolory = new string[rozmiarTablicy];
-        
-            
-            //Console.Clear();
-                       
+            string[] kolory = new string[x];
+
+
+            Console.Clear();
+
             Console.WriteLine("Rozmiar tablicy: " + rozmiarTablicy);
-            
+
             for (int i = 0; i < x; i++)
             {
                 int pom = i + 1;
-                Console.Write("Podaj wartość tablicy nr: " + pom);
+                Console.Write("Podaj wartość tablicy nr " + pom + ":   ");
+
+                string wartosc = Console.ReadLine();
+               // i = int.Parse(i);
+                kolory[i] = wartosc;
+
+               
+                
+
+            }
+
+            Console.WriteLine("\nwyświetlenie tablicy: ");
+
+            for (int j = 0; j < x; j++)
+            {
+                int pom = j + 1; ;
+                Console.WriteLine(pom + " - element tablicy: " + kolory[j]);
+            }
+
+
+            //minimalna wartość
+
+            
+            
+
+            int[] e = new int[kolory.Length];
+
+            for (int f = 0; f < e.Length; f++)
+            {
+                
             }
 
 
 
+            /* int calkowita = int.Parse(kolory[0]);
+             int min = calkowita;
+             for (int k = 1; k < kolory.Length; k++)
+             {
+                 int calkowita1 = int.Parse(kolory[k]);
+                 if (calkowita1 < min)
+                 {
+                     min = kolory[k];
+                     Console.WriteLine("Minimalna wartość tablicy kolory wynosi: " + min);
+
+                 }
+             }*/
+
+
+            
+/*
+            string test1 = kolory[0];
+            string test2 = kolory[1];
+            int testint1 = int.Parse(test1);
+            int testint2 = int.Parse(test2);
+
+            if (testint1 < testint2)
+            {
+                Console.WriteLine("1 jest mniejsze od 2");
+            } else
+            {
+                Console.WriteLine("dwa jest wieksze");
+            }
+            */
 
 
 
@@ -46,6 +105,9 @@ namespace Toci.Training.MirekSz
 
 
 
+
+
+            //**************************************************************************************************
             //tablice
 
 
