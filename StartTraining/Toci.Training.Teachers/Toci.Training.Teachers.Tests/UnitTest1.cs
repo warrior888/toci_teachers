8bb497c343c00b6a9a4be89998f8a1ff6b6e2758
MKafar <mlotek92@gmail.com>
2017-06-30 00:19:37 +0200

    TestApp in progress

--- StartTraining/Toci.Training.Teachers/Toci.Training.Teachers.Tests/UnitTest1.cs
+++ StartTraining/Toci.Training.Teachers/Toci.Training.Teachers.Tests/UnitTest1.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Diagnostics;
+using System.IO;
 using System.Reflection;
+using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Toci.Training.Teachers.Tests
@@ -14,11 +16,20 @@ namespace Toci.Training.Teachers.Tests
             //Process.Start(@"..\..\..\Toci.Training.Kafar\bin\Debug\Toci.Training.Kafar.exe");
             string path =
                 @"C:\Users\mlote\Documents\GitHub\toci_teachers\StartTraining\Toci.Training.Teachers\Toci.Training.Kafar\bin\Debug\Toci.Training.Kafar.exe";
-            const string exe = "Toci.Training.Kafar.exe";
-            var exePath = System.IO.Path.GetFullPath(exe);
-            var universalPath = System.IO.Path.GetDirectoryName(path);
+            const string exe = "costam";
+            string pathX = @"..\..\..\";
+            string[] pathY = new string[50];
+            string[] paths = new string[100];
+            paths = Directory.GetDirectories(pathX);
+            pathY = Directory.GetDirectories(paths[13]);
+            //Directory.
+            var exePath = Path.GetFullPath(exe);
+            var universalPath = Path.GetDirectoryName(exePath);
             ProcessStartInfo testProcessStartInfo = new ProcessStartInfo();
+            string path2 = Assembly.GetExecutingAssembly().Location;
+            string path3 = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
 
+            
 
             string test = Process.GetCurrentProcess().Modules.ToString();
         }
