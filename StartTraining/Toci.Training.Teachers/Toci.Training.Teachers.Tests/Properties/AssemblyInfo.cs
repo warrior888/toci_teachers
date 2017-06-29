03ca17ae12d79e59685ba719f35783f679ba291c
MKafar <mlotek92@gmail.com>
2017-06-29 19:44:37 +0200

    LOOK IF YOUR PROJECT IS MISSING

---
+++ StartTraining/Toci.Training.Teachers/Toci.Training.Teachers.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Toci.Training.Teachers.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Toci.Training.Teachers.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("86d62bec-076e-4f41-b6cc-fd2b5fe5b038")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ StartTraining/Toci.Training.Teachers/Toci.Training.Teachers.Tests/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Toci.Training.Teachers.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Process.Start(@"..\..\..\Toci.Training.Kafar\bin\Debug\Toci.Training.Kafar.exe");
+            string path =
+                @"C:\Users\mlote\Documents\GitHub\toci_teachers\StartTraining\Toci.Training.Teachers\Toci.Training.Kafar\bin\Debug\Toci.Training.Kafar.exe";
+            const string exe = "Toci.Training.Kafar.exe";
+            var exePath = System.IO.Path.GetFullPath(exe);
+            var universalPath = System.IO.Path.GetDirectoryName(path);
+            ProcessStartInfo testProcessStartInfo = new ProcessStartInfo();
+
+
+            string test = Process.GetCurrentProcess().Modules.ToString();
+        }
+    }
+}
