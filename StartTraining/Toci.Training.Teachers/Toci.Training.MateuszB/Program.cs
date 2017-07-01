fb91442d3c73848aef9c7d2003f949238cf5fbfd
Mateusz Buczkowski <mateuszbuczkowski93@gmail.com>
2017-07-01 13:53:48 +0200

    Wstepny quiz o Liverpoolu :)

--- StartTraining/Toci.Training.Teachers/Toci.Training.MateuszB/Program.cs
+++ StartTraining/Toci.Training.Teachers/Toci.Training.MateuszB/Program.cs
@@ -10,7 +10,76 @@ namespace Toci.Training.MateuszB
     {
         static void Main(string[] args)
         {
-            int[] numValues = { 3, 56, 3, 5, 78, 9, 78, 4, 3, 567, 8};
+            int quizAnswersIndex = 0;
+            string answer = "";
+            int countWrong = 0;
+            int countCorrect = 0;
+
+            string[] quizQuestions =
+            {
+                "W którym roku powstał klub?",
+                "Jak nazywa się obecny stadion Liverpoolu?",
+                "Ile mistrzostw mają na koncie The Reds?",
+                "Kiedy ostatni raz zdobyli Ligę Mistrzów?",
+                "Ile lat Steven Gerrard spędził w Liverpoolu",
+                "I na koniec kto jest obecnym trenerem The Reds?",
+            };
+
+            string[][] quizAnswers = new string[quizQuestions.Length][];
+
+            quizAnswers[quizAnswersIndex++] = new string[] { "a) 1905", "b) 1892", "c) 1899" };
+            quizAnswers[quizAnswersIndex++] = new string[] { "a) Upton Park", "b) Stamford Bridge", "c) Anfield" };
+            quizAnswers[quizAnswersIndex++] = new string[] { "a) 19", "b) 18", "c) 20" };
+            quizAnswers[quizAnswersIndex++] = new string[] { "a) 1999", "b) 2007", "c) 2005" };
+            quizAnswers[quizAnswersIndex++] = new string[] { "a) 17", "b) 20", "c) 13" };
+            quizAnswers[quizAnswersIndex++] = new string[] { "a) Brendan Rogers", "b) Jurgen Klopp", "c) Jose Mourinho" };
+
+            string[] correctAnswers = { "b", "c", "b", "c", "a", "b" };
+
+            Console.WriteLine("Witaj w kursie wiedzy na temat Liverpool'u :)");
+            Console.WriteLine("Odpowiadasz poprzez wybranie a, b lub c");
+            Console.WriteLine("Aby zacząć wciśnij ENTER");
+            Console.WriteLine("");
+
+            Console.ReadKey();
+
+            for (int i = 0; i < quizQuestions.Length; i++)
+            {
+                Console.WriteLine(quizQuestions[i]);
+                for (int j = 0; j < quizAnswers[i].Length; j++)
+                {
+                    Console.WriteLine(quizAnswers[i][j]);
+                }
+                Console.WriteLine("");
+                Console.Write("Twoja odpowiedź: ");  answer = Console.ReadLine();
+                Console.WriteLine("");
+
+                while (answer != correctAnswers[i])
+                {
+                    countWrong++;
+                    Console.WriteLine("Niepoprawna odpowiedź! Spróbuj ponownie.");
+                    Console.WriteLine("");
+                    Console.Write("Twoja odpowiedź: "); answer = Console.ReadLine();
+                }
+                countCorrect++;
+                Console.WriteLine("Brawo! Poprawna odpowiedź.");
+                Console.WriteLine("");
+                Console.WriteLine("Wciśnij dowolny klawisz aby przejść do następnego pytania");
+                Console.ReadKey();
+                Console.WriteLine("");
+                Console.Clear();
+            }
+            Console.WriteLine("Twój wynik to: " + countCorrect + " na " + quizAnswers.Length + " możliwych");
+            Console.WriteLine("Zle odpowiedzi: " + countWrong);
+            Console.WriteLine("Dobre odpowiedzi: " + countCorrect);
+
+            Console.ReadKey();
+
+
+            
+            
+            
+            /*int[] numValues = { 3, 56, 3, 5, 78, 9, 78, 4, 3, 567, 8};
             int max = numValues[0], min = numValues[0], sum = 0, number = numValues[4], count = 0; 
 
             for (int i = 0; i < numValues.Length; i++)
