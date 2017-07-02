7fa49b28972b36366dd665cf4cdcdf730a635094
DESKTOP-BVK87II\Łukasz <skorupka813@gmail.com>
2017-07-02 17:05:46 +0200

    Last edit

--- StartTraining/Toci.Training.Teachers/ConsoleApp1/Program.cs
+++ StartTraining/Toci.Training.Teachers/ConsoleApp1/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,7 +15,7 @@ namespace ConsoleApp1
             // zad 1&2&3. Max wartosc/ Min wartosc. Suma liczb z tablicy
             int[] parameters = { 2, 3, 5, 4, 5, 2, 1, 3, 6, 7, 9, 11, 24, 5, 12, 12, 14, 3 };
             int length = parameters.Length;
-            int resultMax = 0, resultMin = 0, sumOfNumber = 0;
+            int resultMax = int.MaxValue, resultMin = int.MinValue, sumOfNumber = 0;
 
 
             for (int i = 0; i < length; i++)
@@ -68,7 +70,7 @@ namespace ConsoleApp1
             for (int j = 0; j < lenghtOfNumber; j++)
             {
                 Console.WriteLine("Podaj liczbe: {0}", j + 1);
-                values[j] = Convert.ToInt32(Console.ReadLine());
+                //values[j] = Convert.ToInt32(Console.ReadLine());
                 
 
 
@@ -77,8 +79,14 @@ namespace ConsoleApp1
             }
 
             Console.WriteLine("Max to: {0} Min to: {1}", resultMax, resultMin);
-            Console.Write("Liczby od użytkownika: {0}", values); // wywala int 32, cos zle zrobilem
+            //Console.Write("Liczby od użytkownika: {0}", values); // wywala int 32, cos zle zrobilem
             Console.ReadLine();
+
+            //Sortowanie babelkowe
+
+            
+
+
         }
     }
 }
