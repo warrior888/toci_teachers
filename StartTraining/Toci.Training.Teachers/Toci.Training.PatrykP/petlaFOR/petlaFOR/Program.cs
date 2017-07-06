ad46789006cfdbaa8fcede3f290327423d05e35b
Patryk Pacak <patryk.pacak@gmail.com>
2017-07-06 19:17:15 +0200

    no message

--- StartTraining/Toci.Training.Teachers/Toci.Training.PatrykP/petlaFOR/petlaFOR/Program.cs
+++ StartTraining/Toci.Training.Teachers/Toci.Training.PatrykP/petlaFOR/petlaFOR/Program.cs
@@ -10,42 +10,11 @@ namespace petlaFOR
     {
         static void Main(string[] args)
         {
-            //string[] wpisane = new string[5];
-
-            //string[] wpisane = new string[5];
-            //string zmienna;
-            //zmienna = wpisane[0];
-            //Console.WriteLine("Podaj coś: ");
-            //zmienna = Console.ReadLine();
-
-            //int limit = wpisane.Length;
-
-
-            //for (int i = 1; i > wpisane.Length; i++  ) 
-            //{
-            //    Console.WriteLine("Podaj coś człowieku:  ") ;
-            //    Console.ReadLine();
-            //    Console.WriteLine("Zapisano pomyślnie" + i);
-
-
-
-
-            // }
-
-            //Console.ReadKey();
-            
-
             string[] pamiec = new string[5];
-           // pamiec[0] = "ala";
-            //pamiec[1] = "kot";
-            //pamiec[2] = "alamakota";
-            //pamiec[3] = "kotmalae";
-            //pamiec[4] = "kotek";
 
-            //Console.WriteLine(pamiec[2]);
-            for (int i = 0; i < pamiec.Length; i++ )
+            for (int i = 0; i < pamiec.Length; i++)
             {
-                Console.WriteLine("Podaj " + i + " informację: " );
+                Console.WriteLine("Podaj " + i + " informację: ");
                 pamiec[i] = Console.ReadLine();
             }
             Console.WriteLine("Dziękujemy, to już wszystkie dane których potrzebowaliśmy!");
