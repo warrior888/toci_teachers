045932ccc8f4cd951b5d777a262193c239db2010
ersoft79 <ersoft66@gmail.com>
2017-07-06 12:09:15 +0200

    no message

--- StartTraining/Toci.Training.Teachers/maksymalnaWartośćTablicy/Program.cs
+++ StartTraining/Toci.Training.Teachers/maksymalnaWartośćTablicy/Program.cs
@@ -8,17 +8,42 @@ namespace maksymalnaWartośćTablicy
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args)           
+
         {
+            //deklaracja zmiennych celowo na samej górze, żeby uniknąć 
+            //śmieci w kodzie ( jeśli źle rozumuję, to poprawcie mnie ;) )
+            int i, max;
             int[] tablica = new int[10];
+
+            //klasa generująca liczby pseudolosowe
             Random r = new Random();
            
-
-            for (int i = 0;i < tablica.Length; i++)
+            
+            //wypełniam tablicę liczbami pseudolosowymi
+            for (i = 0;i < tablica.Length; i++)
             {
                 tablica[i] =r.Next(10);
                 Console.Write(tablica[i] + " ");
             }
+
+            //Przejście do nowej linii
+            Console.WriteLine();
+            max = tablica[9];
+
+            for (i = 0; i < tablica.Length; i++)
+            {
+                if (tablica[i] > max)
+                {
+                    max = tablica[i];
+                }
+              
+            }
+            Console.WriteLine("Największy element tablicy to: " + max);
+
+
+
+
             Console.ReadKey();
         }
      
