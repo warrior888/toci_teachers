3b66749c26d0ce7b07f002a1bfb9b925392bd4ab
alexandergdapl <a@alexander.gda.pl>
2017-07-10 23:33:39 +0200

    dodanie swojego katalogu z projektem Toci_training: Olek_Szycik

---
+++ StartTraining/Paladyni/Olek_Szycik/Toci_training/Toci_training/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Toci_training
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wpisz tekst: ");
+            string textFromUserImput = Console.ReadLine();
+
+            Console.WriteLine("długość tekstu = {0}", textFromUserImput.Length);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
