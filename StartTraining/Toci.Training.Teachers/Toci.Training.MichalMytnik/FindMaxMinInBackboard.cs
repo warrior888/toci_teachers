bc3b084a3b1f5b880fcaa6ed05ef2d5baf39d708
myteksss <michalmyt@interia.pl>
2017-07-12 21:35:41 +0200

    Saturday training tasks

---
+++ StartTraining/Toci.Training.Teachers/Toci.Training.MichalMytnik/FindMaxMinInBackboard.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toci.Training.MichalMytnik
+{
+    class FindMaxMinInBackboard
+    {
+
+        static void MaxMin(string[]args)
+        {
+            int[] element = new int[] { 100, 34, 64, 23, 27, 112, 34, 545, 64, 43, 27 };
+            int maxValuue = element[0];
+            int minValue = element[0];
+            int numberOfRepetitions = element[0];
+            int sumValue = 0;
+
+            for (int i = 0; i < element.Length; i++)
+            {
+
+
+                if (maxValuue < element[i])
+                {
+                    maxValuue = element[i];
+                    // Console.WriteLine("Przypisz nową wartość: " + maxValuue);
+                }
+                if (minValue > element[i])
+                {
+                    minValue = element[i];
+                }
+                sumValue += element[i];
+                //if (numberOfRepetitions == element[i])
+                //{
+
+
+                //}
+            }
+            Console.WriteLine("Max wartość w tablicy to: " + maxValuue);
+            Console.WriteLine("Min wartość w tablicy to: " + minValue);
+            Console.WriteLine("Suma tablicy: " + sumValue);
+            Console.ReadKey();
+
+        }
+
+
+
+    }
+}
