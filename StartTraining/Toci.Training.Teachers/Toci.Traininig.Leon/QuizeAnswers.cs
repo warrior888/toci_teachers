0085bae534cbe2e7946dabaaa4c690187e8aba4a
Home\Home PL <ucios@o2.pl>
2017-07-12 09:39:48 +0200

    New try for object codding

---
+++ StartTraining/Toci.Training.Teachers/Toci.Traininig.Leon/QuizeAnswers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toci.Traininig.Leon
+{
+    public class QuizeAnswers : QuizeQuestion
+    {
+        
+        //string[][] QuizeAnswer = new string[QuizeQuestions.Length][];
+
+    }
+}
