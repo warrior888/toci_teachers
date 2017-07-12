d53d4f09084806fe52674eb42e05f7d19b707ffa
alexandergdapl <a@alexander.gda.pl>
2017-07-12 17:52:50 +0200

    poprawka

--- StartTraining/Paladyni/Olek_Szycik/Toci_training/Arrays/Program.cs
+++ StartTraining/Paladyni/Olek_Szycik/Toci_training/Arrays/Program.cs
@@ -9,12 +9,13 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
-            // zrób tablicę, znajdź najw. wartość i minimalną, suma
+            // zrób tablicę, znajdź wartości min i max, sumę
             // zliczyć ilość wystąpień przykładowego elementu
 
-            int[] arr = new int[] {10, 55, 84, 11, 77, 17, 84, 56, 78};
+                        
+            int[] arr = {70, 55, 11, 32, 58, 73, 48, 61, 88, 99};
 
-            Console.Write("liczby w tablicy: ");
+            Console.Write("liczby w tablicy: \n");
             for (int i = 0; i < arr.Length; i++)
             {
                 Console.Write("{0} {1}", arr[i], " ");
@@ -23,8 +24,10 @@ namespace Arrays
             
             for (int i = 0; i < arr.Length; i++)
             {
-                int min = arr[i];
-                int k = i;
+                int min, k;
+
+                min = arr[i];
+                k = i;
                 for (int j = i + 1; j < arr.Length; j++) 
                 {
                     if (min > arr[j])
@@ -37,7 +40,7 @@ namespace Arrays
                 }
             }
 
-            Console.WriteLine("\nposortowane: ");
+            Console.WriteLine("\n\nposortowane: ");
 
             for (int i = 0; i < arr.Length; i++)
             {
@@ -45,9 +48,25 @@ namespace Arrays
             }
 
             
-            
-            //http://blog.przemyslawsobolewski.com/sortowanie-w-c-dwa-sposoby/
+            Console.WriteLine("\n\nposortowane metodą Array.Sort");
+            Array.Sort(arr);
 
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write("{0} {1}", arr[i], " ");
+            }
+
+
+            Console.WriteLine();
+
+            Array.Reverse(arr);
+
+            Console.WriteLine("\n\nposortowane odwrotnie metodą Array.Reverse");
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write("{0} {1}", arr[i], " ");
+            }
 
 
 
