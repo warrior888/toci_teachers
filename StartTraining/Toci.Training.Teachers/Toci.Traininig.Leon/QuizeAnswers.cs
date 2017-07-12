47ad92c669fc522ad80a8b1bb0795681659d7533
MKafar <mlotek92@gmail.com>
2017-07-12 19:43:46 +0200

    Comments for Leon

--- StartTraining/Toci.Training.Teachers/Toci.Traininig.Leon/QuizeAnswers.cs
+++ StartTraining/Toci.Training.Teachers/Toci.Traininig.Leon/QuizeAnswers.cs
@@ -8,8 +8,20 @@ namespace Toci.Traininig.Leon
 {
     public class QuizeAnswers : QuizeQuestion
     {
+        // Nie wiadomo w jakiej kolejności będą budowane obiekty klas, dlatego QuizeQuestions
+        // może zaistnieć po próbie budowy QuizeAnswer, a do budowy QuizeAnswer potrzebujemy
+        // QuizeQuestions.Lenght.
+        // Nie znamy długości tablicy, która jeszcze nie istnieje.
+
+        // string[][] QuizeAnswer = new string[QuizeQuestions.Length][];
         
-        //string[][] QuizeAnswer = new string[QuizeQuestions.Length][];
+        // Konstruktory z kolei wywoływane są przy tworzeniu obiektu klasy, a rzeczy stworzyć obiekt to
+        // muszą istnieć wszystkie pola klasy. Dlatego budując QuizeAnswer w konstruktorze mamy już długość
+        // QuizeQuestions, ponieważ ta tablica została zbudowana wcześniej.
+        QuizeAnswers()
+        {
+            string[][] QuizeAnswer = new string[QuizeQuestions.Length][];
+        }
 
     }
 }
