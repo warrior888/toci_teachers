4958002c417a268afe79c4c2af254e4feadc6575
Patryk Pacak <patryk.pacak@gmail.com>
2017-07-15 08:50:53 +0200

    commit

--- StartTraining/Toci.Training.Teachers/Toci.Training.PatrykP/Program.cs
+++ StartTraining/Toci.Training.Teachers/Toci.Training.PatrykP/Program.cs
@@ -10,56 +10,57 @@ namespace petlaFOR
     {
         static void Main(string[] args)
         {
-            ////Petla FOR
-            //string[] pamiec = new string[5];
+            //Petla FOR
+            string[] pamiec = new string[5];
 
-            //for (int i = 0; i < pamiec.Length; i++)
-            //{
-            //    Console.WriteLine("Podaj " + i + " informację: ");
-            //    pamiec[i] = Console.ReadLine();
-            //}
-            //Console.WriteLine("Dziękujemy, to już wszystkie dane których potrzebowaliśmy!");
-            //Console.ReadKey();
+            for (int i = 0; i < pamiec.Length; i++)
+            {
+                Console.WriteLine("Podaj " + i + " informację: ");
+                pamiec[i] = Console.ReadLine();
+            }
+            Console.WriteLine("Dziękujemy, to już wszystkie dane których potrzebowaliśmy!");
+            Console.ReadKey();
 
-            ////Translator Kodu ASCII
+            //Translator Kodu ASCII
 
-            //Console.WriteLine("Podaj coś człowieku");
-            //string tekst = Console.ReadLine();
+            Console.WriteLine("Podaj coś człowieku");
+            string tekst = Console.ReadLine();
 
-            //foreach (char znak in tekst)
-            //{
-            //    int i = znak;
+            foreach (char znak in tekst)
+            {
+                int i = znak;
 
-            //    if (znak < 97)
-            //    {
-            //        Przelicz(i);
-            //        char c = Convert.ToChar(Przelicz(i));
+                if (znak < 97)
+                {
+                    Przelicz(i);
+                    char c = Convert.ToChar(Przelicz(i));
 
-            //        Console.WriteLine(c);
-            //    }
-            //    if (znak > 97)
-            //    {
-            //        Przelicz2(i);
-            //        char a = Convert.ToChar(Przelicz2(i));
-            //        Console.WriteLine(a);
-            //    }
+                    Console.WriteLine(c);
+                }
+                if (znak > 97)
+                {
+                    Przelicz2(i);
+                    char a = Convert.ToChar(Przelicz2(i));
+                    Console.WriteLine(a);
+                }
 
-            //    Console.ReadKey();
-            //}
+                Console.ReadKey();
+            }
 
 
 
 
             //Zadanie domowe tydzień 1
-          
-            int[] tablica = new int[] {10,11,11,42,12,15,12,51,12,25,43,13,251,45,3,2,234,32,1,2};
-            int max = 0;
-            int min = 0;
+
+
+            int[] tablica = new int[] { 10, 11, 11, 42, 12, 15, 12, 51, 12, 25, 43, 13, 251, 45, 3, 2, 234, 32, 1, 2 };
+            int max = int.MinValue;
+            int min = int.MaxValue;
             int suma = 0;
             int powtorzenie = 0;
-                for(int i=0; i<tablica.Length; i++)
+            for (int i = 0; i < tablica.Length; i++)
             {
-               
+
                 if (max < tablica[i])
                 {
                     max = tablica[i];
@@ -68,20 +69,47 @@ namespace petlaFOR
                 {
                     min = tablica[i];
                 }
-                suma = tablica[i] + suma; 
+                suma = tablica[i] + suma;
 
                 if (tablica[i] == 11)
                 {
                     powtorzenie++;
                 }
-                
+
             }
             Console.WriteLine("Maksymalna wartość to: " + max);
             Console.WriteLine("Minimalna wartość to:" + min);
             Console.WriteLine("Suma wszyskich elementów wynosi: " + suma);
             Console.WriteLine("Powtórzeń liczby 11: " + powtorzenie);
             Console.ReadKey();
+
+
+            //homework week2
+            // wyciaganie tekstu ze stringu
+
+           // string[] tekst = new string[] { "Patryk czasem programuje" };
+            //string[] slowo = new string[] { "czasem" };
+            //int dlugosc = 12;
+
+           // for (int i = 0; i < tekst.Length; i++)
+           // {
+          //      for (int j = 0; j < tekst.Length; j++)
+           //     {
+          //          if (tekst[i] = slowo)
+          //          {
+
+          //          }
+          //      }
+            //}
+
+
         }
+
+
+
+
+
+
 
         private static int Przelicz(int i)
         {
@@ -100,4 +128,4 @@ namespace petlaFOR
 
 
 
-   
+
