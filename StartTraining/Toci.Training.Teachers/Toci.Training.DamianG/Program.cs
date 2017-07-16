f7a245fd323d47ca65397e437d9ede6a3ddfdbea
LaurentVizz <vizzini98@gmail.com>
2017-07-16 18:44:26 +0200

    Damian G

--- StartTraining/Toci.Training.Teachers/Toci.Training.DamianG/Program.cs
+++ StartTraining/Toci.Training.Teachers/Toci.Training.DamianG/Program.cs
@@ -10,6 +10,23 @@ namespace Toci.Training.DamianG
     {
         static void Main(string[] args)
         {
+            int[] lengthOfStrings = new int[10];
+            int limit = lengthOfStrings.Length;
+            for (int i = 0; i < limit; i++)
+            {
+                Console.WriteLine("Wpisz cokolwiek by pokazało się kilka razy");
+
+
+            }
+
+            Console.WriteLine("Podaj słowo do odwrócenia");
+            string yourName = Console.ReadLine();
+            for (int i = yourName.Length - 1; i >= 0; i--)
+            {
+                Console.Write(yourName[i]);
+            }
+
+
             Console.WriteLine("Wpisz swoje imie");
             //string name;
             string name = Console.ReadLine();
@@ -17,12 +34,31 @@ namespace Toci.Training.DamianG
             string upperString = name.ToUpper();
             Console.WriteLine(upperString);
 
-            Console.WriteLine("4 >5 Prawda czy fałsz?");
+            Console.WriteLine("4 > 5 Prawda czy fałsz?");
             Console.ReadLine();
             bool value = 4 > 5;
             Console.WriteLine(value);
 
             int trueValue = "Damian " == "damian " ? 7 : 8;
+
+            Console.WriteLine("Podaj wyraz a oblicze Ci jego długość");
+            string word = Console.ReadLine();
+            int count = word.Length;
+            Console.WriteLine(count);
+
+            Console.WriteLine("Czy twoje hasło jest wystarczająco długie?");
+            string pass = Console.ReadLine();
+            if (pass.Length >= 8)
+            {
+                Console.WriteLine("Hasło jest wspaniale długie");
+            }
+            else 
+            {
+                Console.WriteLine("Popracuj nad swoją długością hasła");
+            }
+
+
+
             Console.ReadKey();
         }
     }
