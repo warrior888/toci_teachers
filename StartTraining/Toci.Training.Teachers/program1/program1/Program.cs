d18e8487a735163ca2fbdb97ed0742f5adf965db
Artur Grzyb <grzyb.artur@outlook.com>
2017-07-18 19:41:02 +0200

    poczatkowe zadania z tablic

---
+++ StartTraining/Toci.Training.Teachers/program1/program1/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //cztery zadania z tablicami
+
+            Console.WriteLine("Zadanie domowe z tablicami");
+            int[] tablica = { 3, 56, 3, 5, 78, 9, 78, 4, 3, 567, 8 };
+            Console.WriteLine("Zadania na min i max z tablicy");
+
+            // Ustalenie wartości minimalnej
+            int min = tablica[0];
+            for (int i = 1; i < tablica.Length; i++)
+            {
+                if (min > tablica[i])
+                {
+                    min = tablica[i];
+                }
+            }
+            Console.WriteLine("Wartosc minimalna to: " + min);
+            Console.ReadKey();
+
+            // Ustalenie wartości maksymalnej
+            int max = tablica[0];
+            for (int j = 1; j < tablica.Length; j++)
+            {
+                if (max < tablica[j])
+                {
+                    max = tablica[j];
+                }
+
+            }
+            Console.WriteLine("Wartosc maksymalna to: " + max);
+            Console.ReadKey();
+
+            // Ustalenie sumy elementów tablicy
+            int suma = 0;
+            for (int k = 0; k < tablica.Length; k++)
+            {
+                suma = suma + tablica[k];
+            }
+            Console.WriteLine("Suma elementów tablicy to " + suma);
+            Console.ReadKey();
+
+            // Ustalenie zliczeń występowania podanej liczby w tablicy
+            Console.WriteLine("Zliczanie wystąpień elementu w tablicy");
+            Console.WriteLine("Wprowadz dowolna liczbe");
+            string exampleElement = Console.ReadLine();
+            int example = int.Parse(exampleElement);
+            int countofexample = 0;
+            for (int l = 0; l < tablica.Length; l++)
+            {
+                if (tablica[l] == example)
+                    countofexample++;
+            }
+            Console.WriteLine("Podana wartość miała następującą liczbę wystąpień: " + countofexample);
+            Console.ReadKey();
+
+
+            //wpisywanie tekstu do tablicy
+
+            string[] tekst = new string[10];
+            for (int i = 0; i < tekst.Length; i++)
+            {
+                int x = i + 1;
+                Console.WriteLine("Wpisz słowo nr " + x);
+                tekst[i] = Console.ReadLine();
+            }
+            for (int j = 0; j < tekst.Length; j++)
+            {
+                Console.WriteLine(tekst[j]);
+            }
+            Console.WriteLine("Chyba sie udalo xd");
+            Console.ReadKey();
+
+
+        }
+    }
+}
