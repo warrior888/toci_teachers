aec79900d1db87c6d5cf91921ea67df83410ebdd
Darek73 <dkania@op.pl>
2017-07-21 17:18:07 +0200

    zrobione dk

--- StartTraining/Toci.Training.Teachers/Toci.Training.DarekK/zadaniadarekk/zadaniadarekk/Program.cs
+++ StartTraining/Toci.Training.Teachers/Toci.Training.DarekK/zadaniadarekk/zadaniadarekk/Program.cs
@@ -11,12 +11,12 @@ namespace zadaniadarekk
     {
         static void Main(string[] args)
         {
-            int[] dane = new int[] {1, 5, 10, 40, 80, 2, 4, -1, 15, 100, 12, 10, 0};
+            int[] dane = new int[] {1, 5, 10, 40, 80, 2, 4, -1, 15, 10, 100, 12, 10, 0};
             int max = 0;
             int min = 0;
             int suma = 0;
             int naj = 0;
-            int tech = 0;
+            int tech = 10;
             for (int i = 0; i < dane.Length; i++)
             {
                 if (max < dane[i])
@@ -28,13 +28,17 @@ namespace zadaniadarekk
                     min = dane[i];
                 }
                 suma += dane[i];
+                if (tech == dane[i])
+                {
+                    naj++;
+                }
                 
             }
 
             Console.WriteLine("Maksymalna liczba to: " + max);
             Console.WriteLine("Minimalna liczba to: " + min);
             Console.WriteLine("Suma: " + suma); 
-            Console.WriteLine("Powtarzają się "+ naj + " razy");
+            Console.WriteLine(tech + " powtarza się "+ naj + " razy");
             Console.ReadLine();
             
         }
