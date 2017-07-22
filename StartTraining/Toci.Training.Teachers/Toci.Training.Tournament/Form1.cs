e5b704a2435aeb7423f5a020d38593aa0c03818f
ghostrider <bzapart@gmail.com>
2017-07-22 10:55:46 +0200

    tr 4

---
+++ StartTraining/Toci.Training.Teachers/Toci.Training.Tournament/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Toci.Training.Tournament
+{
+    public partial class Form1 : Form
+    {
+        public string[] Participants = new string[10];
+        public int counter = 0;
+        TextBox participantNameTextBox = new TextBox();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //for (int i = 0; i < 10; i++)
+            {
+                AddNewParticipantForm(20);
+            }
+        }
+
+        public void AddParticipant()
+        {
+            //Participants[]
+        }
+
+        public void AddNewParticipantForm(int y)
+        {
+            Label participantLabel = new Label();
+
+            participantLabel.Text = "Dodaj uczestnika: ";
+
+            //participantLabel.BackColor = Color.Red;
+
+            participantLabel.Location = new Point(20, y);
+
+            Controls.Add(participantLabel);
+
+            participantNameTextBox.Location = new Point(150, 20);
+
+            Button participantAdd = new Button();
+
+            participantAdd.Click += ParticipantAdd_Click;
+            participantAdd.Location = new Point(400, 20);
+            participantAdd.Text = "Dodaj";
+
+            Controls.Add(participantAdd);
+            Controls.Add(participantNameTextBox);
+
+            //TextBox
+            //Button
+        }
+
+        private void ParticipantAdd_Click(object sender, EventArgs e)
+        {
+            Participants[counter] = participantNameTextBox.Text;
+            counter++;
+        }
+    }
+}
