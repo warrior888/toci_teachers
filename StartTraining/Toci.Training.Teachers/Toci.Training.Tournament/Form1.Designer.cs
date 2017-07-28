6f64cdb1ec0ff6fd5c6fef2ef8574dc695d2f366
Majkyel <michalkulis1993@gmail.com>
2017-07-28 14:49:12 +0200

    commit

--- StartTraining/Toci.Training.Teachers/Toci.Training.Tournament/Form1.Designer.cs
+++ StartTraining/Toci.Training.Teachers/Toci.Training.Tournament/Form1.Designer.cs
@@ -32,9 +32,10 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1093, 316);
+            this.ClientSize = new System.Drawing.Size(1457, 407);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
