17930eeba43beb26a3848ff22f2cdba0992d90c2
Home\Home PL <ucios@o2.pl>
2017-08-05 12:11:32 +0200

    New folder

---
+++ SundayTraining/Leon/TurnamentApp/TurnamentApp/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TurnamentApp
+{
+    public partial class Form1 : Form
+    {
+        public string[] ParticipantTable = new string[20];
+
+        TurnamentParticipantShuffle oneLove = new TurnamentParticipantShuffle();
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // tu dodawac rozne przyciski
+        public void AddApElements()
+        {
+            
+        }
+    }
+}
