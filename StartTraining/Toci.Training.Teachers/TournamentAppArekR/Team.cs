3024436af637d8220044eea3dfadc9feda6abb89
Arkadiusz Romanowski <tech@globalincome.pl>
2017-08-05 19:32:39 +0200

    MyTournamentApp

---
+++ StartTraining/Toci.Training.Teachers/TournamentAppArekR/Team.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace TournamentAppArekR
+{
+    public class Team
+    {
+        public List<PlayerEntity> Players;
+        public string Name;
+        public int GetScore()
+        {
+            return Players.Sum(player => player.Score);
+        }
+    }
+}
