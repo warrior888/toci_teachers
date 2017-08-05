3d0f284ff33812162dd479216fa286f31de956b8
Darek <darek.b1288@gmail.com>
2017-08-06 01:19:23 +0200

    Tournament/Quiz - zarys aplikacji.

---
+++ StartTraining/Toci.Training.Teachers/Quiz/Group.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Windows.Documents;
+
+namespace Quiz
+{
+    public class Group
+    {
+        public int GroupId { get; private set; }
+        public List<User> GroupMember { get; private set; }
+        public int GroupPoints { get; private set; }
+
+        private const int MaxMembersInGroup = 4;
+
+        public Group()
+        {
+            GroupId++;
+        }
+
+        public bool AddMember(User user)
+        {
+            if (user == null) return false;
+            if (GroupMember.Count <= MaxMembersInGroup)
+            {
+                GroupMember.Add(user);
+                return true;
+            }
+            return false;
+        }
+
+        public void SummGroupPoints()
+        {
+            foreach (var member in GroupMember)
+            {
+                GroupPoints += member.Score;
+            }
+        }
+    }
+}
---
+++ StartTraining/Toci.Training.Teachers/Quiz/QuizManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows.Documents;
+
+namespace Quiz
+{
+    public class QuizManager
+    {
+        public ObservableCollection<User> Users { get; private set; }
+        public List<Group> Groups { get; private set; }
+        public List<QuizToSolve> QuizToSolves { get; private set; }
+
+        public QuizManager()
+        {
+            Users = new ObservableCollection<User>();
+            Groups = new List<Group>();
+            QuizToSolves = new List<QuizToSolve>();
+            // TODO: Dokończyć
+        }
+
+        public bool AddUser(string name)
+        {
+            if (String.IsNullOrEmpty(name)) return false;
+
+            var containsName = from user in Users
+                where user.Name == name
+                select user;
+
+            if (containsName.Any()) return false;
+
+            Users.Add(new User(name));
+            return true;
+        }
+
+        public void CreateGroup(string[] usernames)
+        {
+            var findUsers = from user in Users
+                where usernames.Contains(user.Name)
+                select user;
+
+            Group newGroup = new Group();
+
+            foreach (var user in findUsers)
+                newGroup.AddMember(user);
+
+            Groups.Add(newGroup);
+        }
+    }
+}
