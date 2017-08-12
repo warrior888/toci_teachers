23614e14c4b030a3dea63328c3894b3501164135
budny999 <karolbudnyy@gmail.com>
2017-08-13 00:38:10 +0200

    do sprawdzenia

--- StartTraining/Toci.Training.Teachers/Toci.Training.KarolB/Program.cs
+++ StartTraining/Toci.Training.Teachers/Toci.Training.KarolB/Program.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Security.Cryptography.X509Certificates;
+using System.Text
 using System.Threading.Tasks;
 
 namespace Toci.Training.KarolB
@@ -10,11 +11,13 @@ namespace Toci.Training.KarolB
     {
         static void Main(string[] args)
         {
+
             Console.WriteLine("Mój pierwszy program");
             Console.ReadLine();
 
+            int position = Letterposition("Tygrys", "g");
             string Name = "Karol i  Natalia ";
-            int stringLength= Name.Length;
+            int stringLength = Name.Length;
 
             if (stringLength < 4)
             {
@@ -26,14 +29,37 @@ namespace Toci.Training.KarolB
                 Console.WriteLine("Spełnia sie else");
             }
 
-            for (int i= 0; i < Name.Length; i++)
+            for (int i = 0; i < Name.Length; i++)
             {
                 Console.Write(Name[i]);
             }
-           
+
             Console.WriteLine("\n \n" + stringLength);
             Console.ReadLine();
-           
+
+            string animal = "tygrys";
+
+            for (int i = animal.Length - 1; i >= 0; i--)
+            {
+                Console.Write(animal[i]);
+            }
+            Console.ReadLine();
+
+            string[] animals = new string[5];
+            for (int i = 0; i < animals.Length; i++)
+            {
+                Console.WriteLine("Wpisz zwierze {0}", i);
+                animals[i] = Console.ReadLine();
+
+            }
+
         }
+
+        public static int Letterposition(string animal, string needle)
+        {
+            for (int i = 1; i < animal.Length; i++) ;
+        }
+
     }
+
 }
