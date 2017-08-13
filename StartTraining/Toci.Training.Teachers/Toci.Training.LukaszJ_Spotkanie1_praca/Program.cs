1555d99248b10ed8cceb50d1244d47959dbb6611
ukusera <men1985@wp.pl>
2017-08-13 18:04:17 +0200

    Zadanie domowe z 1 szkolenia

--- StartTraining/Toci.Training.Teachers/Toci.Training.LukaszJ_Spotkanie1_praca/Program.cs
+++ StartTraining/Toci.Training.Teachers/Toci.Training.LukaszJ_Spotkanie1_praca/Program.cs
@@ -10,15 +10,37 @@ namespace Toci.Training.LukaszJ_Spotkanie1_praca
     {
         static void Main(string[] args)
         {
-            #region Zadania domowe
 
-            // zadanie domowe: napisać kod w pętli for, który prosi o linijkę tekstu w bloku kodu i zapamiętuje (wrzuca) ją w tablicy i na koniec wyrzuci podziękowanie za podanie np. 10 elementów do tablicy
+            #region Zadanie 1. Tworzy tablicę 10 elementową z wpisanych słów przez użytkownika i wypisuje ją w konsoli
 
-            // zadanie domowe dla chętnych: napisać translacje duże małe litery na tablicy kodów ascii (toUpper po swojemu)
+            // Napisać kod w pętli for, który prosi o linijkę tekstu w bloku kodu i zapamiętuje (wrzuca) ją w tablicy i na koniec wyrzuci podziękowanie za podanie np. 10 elementów do tablicy
 
+            string[] tableValueFromUser = new string[10];
+            int number = tableValueFromUser.Length;
 
+            for (int i = 0; i < number; i++)
+            {
+                Console.WriteLine("Wprowadź do tablicy wyraz nr " + (i + 1) + ":");
+                tableValueFromUser[i] = Console.ReadLine();
+
+                if (number - 1 == i)
+                {
+                    Console.WriteLine("\n" + "Dziękuję. Uzupełniłeś swoją tablicę 10 elementami.");
+                }
+            }
+
+            Console.ReadKey();
+            Console.WriteLine("\n" + "Tablica została wypełniona następującymi wartościami:");
+
+            for (int i = 0; i < number; i++)
+            {
+                Console.WriteLine((i + 1) + ". " + tableValueFromUser[i]);
+            }
+
+            Console.ReadKey();
 
             #endregion
+
         }
     }
 }
