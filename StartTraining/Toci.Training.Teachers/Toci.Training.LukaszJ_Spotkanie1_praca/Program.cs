749de1bb6e531daef38e262753793e358424eb3f
ukusera <men1985@wp.pl>
2017-08-13 21:51:37 +0200

    Dodatkowe zadania domowe dotyczące pierwszego szkolenia. Tworzenie tablicy o długości zadeklarowanej przez użytkownika, z wartościami wypełnionymi przez niego i wyświetleniem tablicy w konsoli jako zakończenie zadania

--- StartTraining/Toci.Training.Teachers/Toci.Training.LukaszJ_Spotkanie1_praca/Program.cs
+++ StartTraining/Toci.Training.Teachers/Toci.Training.LukaszJ_Spotkanie1_praca/Program.cs
@@ -38,6 +38,65 @@ namespace Toci.Training.LukaszJ_Spotkanie1_praca
             }
 
             Console.ReadKey();
+            Console.WriteLine("\n");
+
+            #endregion
+
+            #region Zadanie 2a. Tworzy tablicę ileś elementową (określoną przez użytkownika) z wpisanych słów przez użytkownika i wypisuje ją w konsoli
+
+            // Kod któy prosi użytkownika o zadeklarowanie wielkości tablicy, wpisanie wartości i wypisanie ich na ekran przy użyciu TryParse
+
+            Console.WriteLine("Ile chcesz umieścić wpisów w tablicy?");
+            string userInput = Console.ReadLine();
+            int userInputNumber = 0;
+            int.TryParse(userInput, out userInputNumber);
+
+            string[] tableDeclaredValueFromUser = new string[userInputNumber];
+            int tableLength = tableDeclaredValueFromUser.Length;
+
+            for (int i = 0; i < tableLength; i++)
+            {
+                Console.WriteLine("Wprowadź do tablicy wpis nr " + (i + 1) + ":");
+                tableDeclaredValueFromUser[i] = Console.ReadLine();
+
+                if (tableLength - 1 == i)
+                {
+                    Console.WriteLine("\n" + "Dziękuję. Uzupełniłeś swoją tablicę {0} elementami.", i + 1);
+                }
+            }
+
+            Console.ReadKey();
+            Console.WriteLine("\n" + "Tablica została wypełniona następującymi wpisami:");
+
+            for (int i = 0; i < tableLength; i++)
+            {
+                Console.WriteLine((i + 1) + ". " + tableDeclaredValueFromUser[i]);
+            }
+
+            Console.ReadKey();
+            Console.WriteLine("\n");
+
+            #endregion
+
+            #region Zadanie 2b. Tworzy tablicę ileś elementową (określoną przez użytkownika) z wpisanych słów przez użytkownika i wypisuje ją w konsoli
+
+            // Kod któy prosi użytkownika o zadeklarowanie wielkości tablicy, wpisanie wartości i wypisanie ich na ekran przy użyciu Convert.ToInt32
+
+            Console.WriteLine("Ile chcesz wpisać imion?");
+            int rozmiar = Convert.ToInt32(Console.ReadLine());
+            string[] imiona = new string[rozmiar];
+
+            for (int i = 0; i < imiona.Length; i++)
+            {
+                Console.WriteLine("Podaj {0} imię", i + 1);
+                imiona[i] = Console.ReadLine();
+            }
+            Console.WriteLine("Imiona, które zostały zapisane: ");
+            for (int i = 0; i < imiona.Length; i++)
+            {
+                Console.WriteLine((i + 1) + ". " + imiona[i]);
+            }
+            Console.ReadKey();
 
             #endregion
 
