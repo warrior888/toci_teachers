d2ec7a04bd2cc0f9d21871697d0700e69c41eda9
budny999 <karolbudnyy@gmail.com>
2017-08-14 01:05:32 +0200

    Pierwsze zadanie domowe (Teachers) (Pętla)

--- StartTraining/Toci.Training.Teachers/Toci.Training.KarolB/Program.cs
+++ StartTraining/Toci.Training.Teachers/Toci.Training.KarolB/Program.cs
@@ -11,6 +11,35 @@ namespace Toci.Training.KarolB
     {
         static void Main(string[] args)
         {
+            string[] fish = new string[10];
+            for (int i = 0; i < fish.Length; i++)
+            {
+             
+                Console.WriteLine("Wpisz nazwe ryby {0} ", i);
+                fish[i] = Console.ReadLine(); 
+
+                if (i == fish.Length - 1)
+
+                {
+                    Console.WriteLine("Dziekuje za uzupełnienie tabeli");
+                }
+                
+               
+            }
+            Console.ReadKey();
+            Console.WriteLine("\n");
+            Console.WriteLine("Tablica Ryb");
+
+            for (int i = 0; i < fish.Length; i++)
+            {
+                Console.WriteLine(""+(i+1)+ "." +fish[i]);
+            }
+
+
+
+
+
+
 
             Console.WriteLine("Mój pierwszy program");
             Console.ReadLine();
