44420cfe5526e872d477925bd0ca9184dfb2671e
ersoft79 <ersoft66@gmail.com>
2017-08-24 17:43:31 +0200

    JarekGie - lekcja pierwsza

--- StartTraining/Toci.Training.Teachers/Toci.Training.JarekGie/Program.cs
+++ StartTraining/Toci.Training.Teachers/Toci.Training.JarekGie/Program.cs
@@ -17,208 +17,59 @@ namespace Toci.Training.JarekGie
     {
         static void Main(string[] args)
         {
-            ///*
-            ////zadanie 1.1 - Dodawanie stringów do tablicy
+            //Zadanie 1.1 - Dodawanie stringów do tablicy
+/*
+            string[] tablica = new string[10];
 
-            //string[] tablicaStringow = new string[10];
+            Console.WriteLine("Wpisz swoją liczbę: ");
 
-
-            //Console.WriteLine("Napisz coś :");
-
-            //for (int i = 0; i < tablicaStringow.Length; i++)
-            //{
-
-            //    tablicaStringow[i] = Console.ReadLine();
-            //    Console.WriteLine("Dodano :" + tablicaStringow[i]);
-            //    if (i == tablicaStringow.Length - 1)
-            //    {
-            //        Console.WriteLine("Koniec tablicy");
-            //    }
-            //}
-
-
-            //for (int i = 0; i < tablicaStringow.Length; i++)
-            //{
-            //    Console.Write(tablicaStringow[i] + " ");
-            //}
-
-            //for (int i = tablicaStringow.Length - 1; i >= 0; i--)
-            //{
-            //    Console.Write(tablicaStringow[i] + " ");
-            //}
-
-
-
-            ////Zadanie 2.1 - Największy element tablicy
-
-
-            //int max;
-            //int[] tablica1 = new int[10];
-
-            ////klasa generująca liczby pseudolosowe
-            //Random r = new Random();
-
-
-            ////wypełniam tablicę liczbami pseudolosowymi
-            //for (int i = 0; i < tablica1.Length; i++)
-            //{
-            //    tablica1[i] = r.Next(10);
-            //    Console.Write(tablica1[i] + " ");
-            //}
-
-            ////Przejście do nowej linii
-            //Console.WriteLine();
-            //max = tablica1[9];
-
-            //for (int i = 0; i < tablica1.Length; i++)
-            //{
-            //    if (tablica1[i] > max)
-            //    {
-            //        max = tablica1[i];
-            //    }
-
-            //}
-            //Console.WriteLine("Największy element tablicy to: " + max);
-
-            ////Zadanie 3.1 - Najmniejszy element tablicy
-
-            //int min;
-            //int[] tablica2 = new int[10];
-
-            ////klasa generująca liczby pseudolosowe
-            //Random _r = new Random();
-
-
-            ////wypełniam tablicę liczbami pseudolosowymi
-            //for (int i = 0; i < tablica2.Length; i++)
-            //{
-            //    tablica2[i] = _r.Next(10);
-            //    Console.Write(tablica2[i] + " ");
-            //}
-
-            ////Przejście do nowej linii
-            //Console.WriteLine();
-            //min = tablica2[9];
-
-            //for (int i = 0; i < tablica2.Length; i++)
-            //{
-            //    if (tablica2[i] < min)
-            //    {
-            //        min = tablica2[i];
-            //    }
-
-            //}
-            //Console.WriteLine("Najmniejszy element tablicy to: " + min);
-
-            ////Zadanie 4.1 - Suma elementów tablicy
-
-            //int wynik = 0;
-
-            ////inicjalizacja tablicy
-            //int[] tab = new int[10];
-
-            ////inicjalizacja losowania
-            //Random r_ = new Random();
-
-            ////wypełnienie tablicy
-            //for (int i = 0; i < tab.Length; i++)
-            //{
-            //    tab[i] = r_.Next(10);
-            //    Console.Write(tab[i] + " ");
-            //}
-
-            ////Pusta linijka
-            //Console.WriteLine();
-
-            ////suma tablicy
-            //for (int i = 0; i < tab.Length; i++)
-            //{
-            //    wynik += tab[i];
-            //}
-            //Console.WriteLine("Suma elementów to: " + wynik);
-            //*/
-
-
-            ////Zadanie 5.1 - zliczanie wystąpień elementów w tablicy
-
-            //int suma,
-            //    szukam;
-
-            int[] elementy = new int[] { 2, 15, 56,78, 45, 3, 7, 8, 9, 11 };
-
-
-            //for (int i = 0; i < elementy.Length; i++)
-            //{
-            //    Console.Write(elementy[i] + " ");
-            //}
-            //suma = 0;
-            //szukam = 2;
-            //for (int i = 0; i < elementy.Length; i++)
-            //{
-            //    if (elementy[i] == szukam)
-            //    {
-            //        suma = suma + 1;
-            //    }
-
-            //}
-            //Console.Write("Szukana wartość występuje " + suma + " razy ");
-
-
-            //sortowanie bąbelkowe
-
-
-
-            //int temp;
-            //for (int i = 0;i < elementy.Length; i++)
-            //{
-            //    for (int j = 0;j < elementy.Length; j++)
-            //    {
-            //        if (i != j)
-            //        {
-            //            if (elementy[i] < elementy[j])
-            //            {
-            //                temp = elementy[i];
-            //                elementy[i] = elementy[j];
-            //                elementy[j] = temp;
-            //            }
-            //        }
-
-            //    }
-
-            //}
-
-            //for (int i = 0; i < elementy.Length; i++)
-            //{
-            //    Console.Write(elementy[i] + " ");
-            //}
-
-            /*
-            //sub string
-
-            string imie = "Jarek";
-
-            for (int i = imie.Length - 1; i >= 0;i--)
+            for(int i = 0; i < tablica.Length; i++)
             {
-                Console.Write(imie[i]);
+                
+                tablica[i] = Console.ReadLine();
 
+                Console.WriteLine("Dodano :" + tablica[i]);
+
+                if (i == tablica.Length -1 )
+                {
+                    Console.WriteLine("Koniec Tablicy");
+                }
+            }
+            for(int i = 0; i < tablica.Length; i++)
+            { 
+                Console.WriteLine(tablica[i] + " ");
             }
             */
-            //Quiz
 
-            string[] quizQuestions = new string[]
+            // Zadanie 2.1 - Największy element tablicy
+
+            int min;
+            int[] tablicaInt = new int[10];
+
+            //klasa generująca liczby pseudolosowe
+            Random r = new Random();
+
+
+            //wypełniam tablicę liczbami pseudolosowymi
+            for (int i = 0; i < tablicaInt.Length; i++)
             {
-                "Czy język C# jest językiem .Net",
-                "Który typ jest typem logicznym?",
-                "Co przechowuje zmienna o typie int?"
-            };
-            string[][] quizAnswers = new string[quizQuestions.Length][];
-            int quizIndexer = 0;
-            quizAnswers[quizIndexer++] = new string[] { "a) tak", "b)nie" };
-            quizAnswers[quizIndexer++] = new string[] { "a) int", "b)bool","c)double","float" };
-            quizAnswers[quizIndexer++] = new string[] { "a) liczby dodatnie", "b)liczby całkowite","c)łańcuchy znaków",
-                                                        "d)pojedynczy znak" };
+                tablicaInt[i] = r.Next(10);
+                Console.Write(tablicaInt[i] + " ");
+            }
 
+            //Przejście do nowej linii
+            Console.WriteLine();
+            min = tablicaInt[9];
 
+            for (int i = 0; i < tablicaInt.Length; i++)
+            {
+                if (tablicaInt[i] < min)
+                {
+                    min = tablicaInt[i];
+                }
+
+            }
+            Console.WriteLine("Najmniejszy element tablicy to: " + min);
             Console.ReadKey();
 
 
