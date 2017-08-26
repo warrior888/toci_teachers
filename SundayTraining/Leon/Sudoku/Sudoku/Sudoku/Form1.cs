419f8eeff79ae0a67b3e46e63b9439caa2119a71
Home\Home PL <ucios@o2.pl>
2017-08-26 08:34:47 +0200

    Sudoku 2.0

--- SundayTraining/Leon/Sudoku/Sudoku/Sudoku/Form1.cs
+++ SundayTraining/Leon/Sudoku/Sudoku/Sudoku/Form1.cs
@@ -14,6 +14,7 @@ namespace Sudoku
     {
         public int[,] SudokuTable = new int[9,9];
         Random Switcher = new Random();
+        Button[,] someButtone = new Button[9,9];
         int switcherNumber;
 
         public Form1()
@@ -35,6 +36,7 @@ namespace Sudoku
 
         public void AddFields(int x, int y, int name)
         {
+            int increment = 1;
             TextBox SudokuField = new TextBox();
             switcherNumber = Switcher.Next(0,2);
             if (switcherNumber != 0)
@@ -45,23 +47,47 @@ namespace Sudoku
             //SudokuField.BorderStyle = Color.Red;
             SudokuField.Location = new Point(20*(1+x),20*(1+y));
             Controls.Add(SudokuField);
+            increment++;
         }
 
         public void AddButton()
         {
-            Button someButtone = new Button();
-            someButtone.Text = "5";
-            someButtone.SetBounds(20,20,20,20);
-            someButtone.Location = new Point(250, 20);
-            Controls.Add(someButtone);
-            someButtone.Click += SomeButtone_Click;
+            for (int x = 0; x < 9; x++)
+            {
+                for (int y = 0; y < 9; y++)
+                {
+                    someButtone[x,y] = new Button();
+                    someButtone[x, y].Text = SudokuTable[x, y].ToString();
+                    someButtone[x, y].SetBounds(20, 20, 20, 20);
+                    someButtone[x, y].Location = new Point(250+(20*x), 20*y);
+                    Controls.Add(someButtone[x, y]);
+                    someButtone[x, y].Click += SomeButtone_Click;
+                }
+            }
         }
 
         private void SomeButtone_Click(object sender, EventArgs e)
         {
-            ListBox NumbersForChoose = new ListBox();
-            NumbersForChoose.Location = new Point(250, 20);
-            Controls.Add(NumbersForChoose);
+            string button = "";
+            string table = "";
+            bool checking = true;
+            for (int x = 0; x < 9; x++)
+            {
+                for (int y = 0; y < 9; y++)
+                {
+                    button = Convert.ToString(someButtone[x, y].Text);
+                    table = SudokuTable[x, y].ToString();
+                    if (button != table)
+                    {
+                        checking = false;
+                    }
+                }
+            }
+
+            if (checking)
+            {
+                MessageBox.Show("Correct");
+            }
         }
     }
 }
---
+++ SundayTraining/Leon/Sudoku/Sudoku/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Wymagana metoda obsługi projektanta — nie należy modyfikować 
+        /// zawartość tej metody z edytorem kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(25, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start Game";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(106, 26);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Restart";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(187, 26);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Check ";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(268, 26);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "End Game";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(430, 412);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
