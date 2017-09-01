4cec82b2613c4dc669cdfb4d9514b29447c9389e
Łukasz Łuczko <lukasz.luczko@gmail.com>
2017-09-01 14:33:21 +0200

    commit after all videos

--- StartTraining/Toci.Training.Teachers/Toci.Training.LukaszLucz/Program.cs
+++ StartTraining/Toci.Training.Teachers/Toci.Training.LukaszLucz/Program.cs
@@ -6,10 +6,53 @@ using System.Threading.Tasks;
 
 namespace Toci.Training.LukaszLucz
 {
+    class CandidateForAnObjectInstance
+    {
+
+        public string QuizQuestion;
+    }
+
+    class QuizAnswer
+    {
+        public string Answer;
+        public bool IsanswerCorrect;
+    }
+
+    class QuizQuestion
+    {
+        public string Question;
+        public QuizAnswer[] Answers;
+    }
+
     class Program
     {
+        //QuizQuestion[] questions = new QuizQuestion[10];    
+
+        public static QuizQuestion[] GetQuizQuestions()
+        {
+            // fill my object with questions
+            return new QuizQuestion[10];
+        }
+
+        public static QuizAnswer[] ShuffleAnswers(QuizQuestion question)
+        {
+            // todo shuffle
+            
+            return question.Answers;
+        }
+
+        public static void ShowQuestions()
+        {
+            
+        }
+        
         static void Main(string[] args)
         {
+            /// Pierwsza aplikacja consolowa dla Toci...
+
+
+
+
         }
     }
 }
