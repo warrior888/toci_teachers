5806338ab76c0087151deaf53dfd23617322f64d
Piotr2\Piotr <pkubon2@gmail.com>
2017-09-03 05:08:40 +0200

    snake V -1 / mechanics

---
+++ SundayTraining/Peter/mySnake/mySnake/myClassesOLDversion/MoveButtons.cs
@@ -0,0 +1,39 @@
+﻿using System.Drawing;
+
+namespace mySnake.myClasses
+{
+    public class MoveButtons
+    {
+        public MoveButtons(Form1 form1)
+        {
+            addButtons(form1);
+        }
+
+        public static int flag = 1;
+        private int MoveButtonSize = 50;
+        private Color Color = Color.BurlyWood;
+
+        private void addButtons(Form1 form1)
+        {
+            var buttonUp = MyButton.make(form1, 350, 300, Color);
+            buttonUp.Text = "UP";
+            buttonUp.Size = new Size(MoveButtonSize, MoveButtonSize);
+            buttonUp.Click += (sender, args) => { flag = -1; };
+
+            var buttonDown = MyButton.make(form1, 350, 400, Color);
+            buttonDown.Text = "DOWN";
+            buttonDown.Size = new Size(MoveButtonSize, MoveButtonSize);
+            buttonDown.Click += (sender, args) => { flag = 1; };
+
+            var buttonRight = MyButton.make(form1, 400, 350, Color);
+            buttonRight.Text = "RIGHT";
+            buttonRight.Size = new Size(MoveButtonSize, MoveButtonSize);
+            buttonRight.Click += (sender, args) => { flag = 2; };
+
+            var buttonLeft = MyButton.make(form1, 300, 350, Color);
+            buttonLeft.Text = "LEFT";
+            buttonLeft.Size = new Size(MoveButtonSize, MoveButtonSize);
+            buttonLeft.Click += (sender, args) => { flag = -2; };
+        }
+    }
+}
---
+++ SundayTraining/Peter/mySnake/mySnake/myClassesOLDversion/MyTable.cs
@@ -0,0 +1,25 @@
+﻿using System.Drawing;
+
+namespace mySnake.myClasses
+{
+    public class MyTable
+    {
+        public static int tableSize = 10;
+
+        public MyTable(Form1 form1)
+        {
+            makeTable(form1);
+        }
+
+        private void makeTable(Form1 form1)
+        {
+            for (int i = 0; i < tableSize; i++)
+            {
+                for (int j = 0; j < tableSize; j++)
+                {
+                    new MyButton(form1, j * MyButton.sizeButton, i * MyButton.sizeButton,Color.Azure);
+                }
+            }
+        }
+    }
+}
