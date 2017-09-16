922aaed9569dc9db09d1d340eec1e09914360d7d
WoSew <wojseweryn@gmail.com>
2017-09-16 15:34:48 +0200

    Graph, Dictionary Szkolenie 16.09.2017

--- StartTraining/Toci.Training.Teachers/DeltaForce/OperationCheckmate/Toci.Teachers.Checkmate/Toci.Teachers.Checkmate/Interfaces/ITociTeachers.cs
+++ StartTraining/Toci.Training.Teachers/DeltaForce/OperationCheckmate/Toci.Teachers.Checkmate/Toci.Teachers.Checkmate/Interfaces/ITociTeachers.cs
@@ -3,6 +3,26 @@ using System.Collections.Generic;
 
 namespace Toci.Teachers.Checkmate.Interfaces
 {
+    public class Graph : Dictionary<Graph, Graph>
+//Dictionary<string, Dictionary<string, string>>
+    {
+        protected int VisitCount;
+
+        public string Name;
+
+        public Dictionary<string, Graph> Neighbours = new Dictionary<string, Graph>();
+
+        public virtual void AddNeighbour( Graph graph)
+        {
+            Graph newOne = Neighbours.ContainsKey(graph.Name) ? Neighbours[graph.Name] : new Graph();
+            newOne.Add(graph, graph);
+            Neighbours.Add(graph.Name, newOne);
+        }
+
+        //public Dictionary<string, Dictionary<string, Dictionary<>>>
+
+    }
+
     public interface ITociTeachers
     {
         
@@ -20,14 +40,15 @@ namespace Toci.Teachers.Checkmate.Interfaces
     public interface IWoman<TWallet, TTalent> : IHuman<IWallet, TTalent>
     {
         // dependency inj
+        // 
     }
 
     public class Woman<TWallet, TTAlent> : IWoman<TWallet, TTAlent> where TWallet : IWallet
     {
         public bool WantsToPee()
         {
-            TWallet t = new TWallet();
-            t.IsRich();
+            ///TWallet t = new TWallet();
+            //t.IsRich();
             throw new NotImplementedException();
         }
 
@@ -80,15 +101,15 @@ namespace Toci.Teachers.Checkmate.Interfaces
         }
     }
 
-    /*public static class HumanExtensions
+    public static class HumanExtensions
     {
-        public static bool WasBirthSuccessfull(this IWoman woman)
+        public static bool WasBirthSuccessfull(this IWoman<IWallet, string> woman)
         {
             return true;
         }
     }
 
-    public class Woman : IWoman
+    public class Woman : IWoman<IWallet, string>
     {
 
         public bool WantsToPee()
@@ -96,14 +117,14 @@ namespace Toci.Teachers.Checkmate.Interfaces
             
             throw new System.NotImplementedException();
         }
-    }*/
+    }
 
 
     public class ThisIsForLEon
     {
         public void dummy()
         {
-            
+            Woman<IWallet, string> w = new Woman<IWallet, string>();
 
             w.WantsToPee();
 
@@ -113,6 +134,30 @@ namespace Toci.Teachers.Checkmate.Interfaces
 
             dafaq.Add("leon");
 
+        }
+    }
+
+    public interface IOperation
+    {
+        string Read();
+    }
+
+    public abstract class OperationBase : IOperation
+    {
+        public string Read()
+        {
+            throw new NotImplementedException();
+        }
+
+        public abstract bool SaveData(string whatever);
+    }
+
+    public class XmlOperation : OperationBase
+    {
+        public override bool SaveData(string whatever)
+        {
+            OperationBase fds = new XmlOperation();
+            throw new NotImplementedException();
         }
     }
 }
--- StartTraining/Toci.Training.Teachers/DeltaForce/OperationCheckmate/Toci.Teachers.Checkmate/Toci.Teachers.Checkmate/Program.cs
+++ StartTraining/Toci.Training.Teachers/DeltaForce/OperationCheckmate/Toci.Teachers.Checkmate/Toci.Teachers.Checkmate/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Toci.Teachers.Checkmate.Interfaces;
 
 namespace Toci.Teachers.Checkmate
 {
@@ -10,6 +11,37 @@ namespace Toci.Teachers.Checkmate
     {
         static void Main(string[] args)
         {
+            Graph gr; 
+
+            for (int i = 0; i < 100; i++)
+            {
+               gr = new Graph();
+                gr.Name = "GR" + i;
+
+                gr.AddNeighbour(gr);
+            }
+
+            
+
+
+            Dictionary<string, string> TociGangstas = new Dictionary<string, string>
+            {
+                { "GR", "Audi r8" },
+                { "WS", "Porsche panamera" },
+            };
+
+            int ja = 1;
+            int max = 8;
+
+            Dictionary<int, Func<int, int, bool> > mapka = new Dictionary<int, Func<int, int, bool>>
+            {
+                //{ ja, (1,1) => {return true }},
+            };
+
+            mapka.Add(ja, (i1, i2) => i1 > i2);
+
+            //nt i = 8;
+
         }
     }
 }
