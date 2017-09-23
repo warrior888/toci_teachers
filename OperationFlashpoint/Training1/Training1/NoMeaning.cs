ee54ea2704df85cbb73798b1b0d70677e0d2a447
ghostrider <bzapart@gmail.com>
2017-09-23 08:59:05 +0200

    tr polimorphism.

---
+++ OperationFlashpoint/Training1/Training1/NoMeaning.cs
@@ -0,0 +1,53 @@
+﻿namespace Training1
+{
+    public abstract class Sex  // ??????
+    {
+        protected bool IsAnAction; //  ????? 
+        // internal private protected public
+    }
+
+    public class Male : Sex
+    {
+        public int Penis { get; set; }
+    }
+
+    public abstract class Blowjob
+    {
+        private Sex SexInAction { get; set; }
+
+        public abstract bool IsInAction();
+    }
+
+    public class UnrecognizedBlowjob : Blowjob
+    {
+        public override bool IsInAction()
+        {
+            return false;
+        }
+    }
+
+    public class ForcedBlowjob : UnrecognizedBlowjob
+    {
+        public override bool IsInAction()
+        {
+            //if () ? 
+            return base.IsInAction();
+        }
+    }
+
+    public class UnexpectedBlowjob : Blowjob
+    {
+        public override bool IsInAction()
+        {
+            return true;
+        }
+    }
+
+    public class Violence
+    {
+        public void TheyWerePunishedForMadonaBlowJobForcing()
+        {
+            //Blowjob bj = 
+        }
+    }
+}
