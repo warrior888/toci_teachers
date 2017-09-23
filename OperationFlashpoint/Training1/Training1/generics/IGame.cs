3130b79c5cb97c11a75b93b2ca926e946623dfc1
ghostrider <bzapart@gmail.com>
2017-09-23 14:00:32 +0200

    games

---
+++ OperationFlashpoint/Training1/Training1/generics/IGame.cs
@@ -0,0 +1,7 @@
+﻿namespace Training1.generics
+{
+    public interface IGame<TPlatform>
+    {
+        TPlatform GetPlatform();
+    }
+}
---
+++ OperationFlashpoint/Training1/Training1/generics/ext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Training1.generics
+{
+    public static class ext
+    {
+        public static bool Wh(this Func<string, string> ob)
+        {
+            return true;
+        }
+
+        public static string Test(string test2)
+        {
+
+            return "";
+        }
+    }
+}
