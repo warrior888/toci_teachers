b60c15703632a95272bfe1dad5e5cc620d998745
ghostrider <bzapart@gmail.com>
2017-09-23 21:51:20 +0200

    oakie dokie 0.0.8

---
+++ OakieDokie/OakieDokie/OakieDokie/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OakieDokie
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            WebClient wc = new WebClient();
+            wc.DownloadFile("http://vps458649.ovh.net/join.me.toci.msi", "C:\\xampp\\test.join.me.msi");
+        }
+    }
+}
