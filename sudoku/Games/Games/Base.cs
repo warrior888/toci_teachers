0a781c5beeaadfabc1c7ea5f11b84aac7ca52724
ghostrider <bzapart@gmail.com>
2017-09-25 21:20:04 +0200

    sudoku

---
+++ sudoku/Games/Games/Base.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Games
+{
+    public partial class Base : Form
+    {
+        public Base()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            GhostRider gr = new GhostRider();
+            gr.Show();
+        }
+    }
+}
