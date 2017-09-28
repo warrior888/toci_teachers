25ff5128147bcc8f408fe33b7b52e49bc44f0166
Łukasz Łuczko <lukasz.luczko@gmail.com>
2017-09-28 11:52:50 +0200

    added Cheesboard by LukaszLu

---
+++ OperationFlashpoint/Training1/Chess/CheesboardByLukaszLu.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Training1;
+
+namespace Chess
+{
+    public partial class CheesboardByLukaszLu : GhostRiderForm
+    {
+        ChessBoard chessboard = new ChessBoard();
+        public CheesboardByLukaszLu()
+        {
+            InitializeComponent();
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    AddControl(chessboard, i, j);
+                }
+            }
+        }
+    }
+}
