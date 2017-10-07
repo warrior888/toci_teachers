8a7b3e114ac11085c6666e98fcaac8f0e00ea6b8
warrior888 <warriorr@poczta.fm>
2017-10-07 12:25:44 +0200

    szkolenie 2

---
+++ Nfs/First/First/AdvancedExampleForPalindrom.cs
@@ -0,0 +1,24 @@
+﻿namespace First
+{
+    public static class AdvancedExampleForPalindrom
+    {
+        public static bool IsPalindrom(string palindromCandidate) // ?????
+        {
+            string palindromCandidateReversed;
+
+            for (int i = palindromCandidate.Length - 1;;) // 
+            {
+                // bartek
+            }
+
+            // kajak 
+
+            if (palindromCandidateReversed == palindromCandidate) // bartek -> ketrab , kajak -> kajak
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
